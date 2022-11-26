43773fae790268d3e5c10b7ba6643f6e48d76756
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-26 23:04:46 +0000

    Amazon Clone Init

--- Src/pages/_app.js
+++ Src/pages/_app.js
@@ -5,9 +5,9 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return 
-  <Provider store = {store}>
+  <Provider store={store}>
       <Component {...pageProps} />
-  </Provider>
+   </Provider>
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode : 'jit',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Src/pages/**/*.{js,ts,jsx,tsx}",
+    "./Src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
