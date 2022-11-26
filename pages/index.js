63b5e23429ab51ad0f3d4b0e1d8d723d9ebb8f73
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-26 21:47:13 +0000

    Amazon Clone Init

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ export default function Home() {
         <title>Amazon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>Hello World</h1>
+      <h1 className='text-2xl font-bold underline text-red-600'>Hello World</h1>
     </div>
   )
 }
