e0b5504bc0e7da32ffdf49d07edd714be59dc627
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-27 00:04:53 +0000

    Amazon Clone Init

--- Src/pages/index.js
+++ Src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../Components/Header'
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className='text-2xl font-bold underline text-red-600'>Hello World</h1>
+
+      <Header />
     </div>
   )
 }
