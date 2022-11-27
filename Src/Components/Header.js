90352989997db57d400f00ad3827bd7c9148d061
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-27 15:08:15 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -24,15 +24,15 @@ function Header() {
     </div>
       {/* right section */}
       <div className='text-white items-center space-x-6 text-xs flex mx-6 whitespace-nowrap'>
-         <div className='text-white items-center space-x-6 text-xs'>
+        <div className='cursor-pointer link'>
     <p>Hello,Desmond Goldsmith</p>
           <p>Account & Lists</p>
     </div>
-    <div >
+    <div className='cursor-pointer link' >
       <p>Return</p>
       <p>& Orders</p>
     </div>
-    <div className='flex items-center'>
+    <div className='cursor-pointer flex items-center link'>
       <ShoppingCartIcon className='h-10' />
       <p>Cart</p>
     </div>
