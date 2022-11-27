f213fb8aae522a11dc9296d326331b1ca2d7f6b6
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-27 14:00:10 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -19,9 +19,16 @@ function Header() {
 
     {/* search bar */}
     <div className='hidden sm:flex items-center rounded-md  flex-grow  cursor-pointer h-10 bg-yellow-400 hover:bg-yellow-500'>
-      <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none' type="text"/>
+      <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' type="text"/>
       <MagnifyingGlassIcon  className = 'text-red-600 h-12 p-4'/>
     </div>
+
+    <div>
+      
+    </div>
+    <div></div>
+    <div></div>
+
     </div>
      
 
