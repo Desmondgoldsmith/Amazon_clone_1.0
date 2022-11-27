838951cfe6784cc3dd33554a02ed33b17159cbf8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-27 12:47:55 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header>
     {/* first section */}
-    <div>
+    <div className='flex items-center bg-amazon_blue'>
        <div>
       <Image src = "https://logos-world.net/wp-content/uploads/2020/04/Amazon-Symbol.jpg" 
       width={100} 
