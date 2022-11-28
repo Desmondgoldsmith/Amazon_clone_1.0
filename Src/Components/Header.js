c717ed0fe1bcd55bb5c461ff11f21f9a12b2c5bf
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-28 13:08:14 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -79,11 +79,16 @@ function Header() {
                     <li className='pt-5 pd-5'><hr/></li>
                 </li>
                 <div className='grid grid-cols-2 divide-x'>
-                    <l>
-                    <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">
+                    <l className = 'break-all'>
+                    <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100  border border-solid border-red-600">
                       <p className='font-extrabold text-lg'>Your Lists</p>
                       <p  className='text-gray-500 text-sm hover:underline hover:text-orange-500'>create a list</p>
                       <p className='text-gray-500 text-sm hover:underline hover:text-orange-500'>find a list or registry</p>
+                      <p className='text-gray-500 text-sm hover:underline hover:text-orange-500'>Amazon smile charity list</p>
+                      <p className='text-gray-500 text-sm hover:underline hover:text-orange-500'>Amazon smile charity list</p>
+                      <p className='text-gray-500 text-sm hover:underline hover:text-orange-500'>Amazon smile charity list</p>
+                      <p className='text-gray-500 text-sm hover:underline hover:text-orange-500'>Amazon smile charity list</p>
+                      <p className='text-gray-500 text-sm hover:underline hover:text-orange-500'>Amazon smile charity list</p>
                       <p className='text-gray-500 text-sm hover:underline hover:text-orange-500'>Amazon smile charity list</p>
                     </a>
                 </l>
