7c4ea2fbc82ce303c589c157ac2692783b309982
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-28 10:54:57 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -16,7 +16,7 @@ function Header() {
          />
        </div>
        {/* delivery location */}
-       <div className='hidden md:inline text-white ml-2 mr-2'>
+       <div className='hidden lg:inline text-white ml-2 mr-2'>
         <p className='text-xs pl-3'>Deliver to</p>
         <p className='flex font-extrabold'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -50,13 +50,13 @@ function Header() {
       <div className='text-white text-xs flex space-x-2 items-center mx-6 whitespace-nowrap'>
       
             {/* language section */}
-      <div className='hidden lg:flex flex mr-2s'>
+      <div className='hidden lg:flex  mr-2 space-x-1'>
         <Image src='https://etc.usf.edu/clipart/72500/72519/72519_28_us_c_lg.gif'
           width={40}
           height={10}
           objectFit='contain'
         />
-        <p className='font-extrabold text-base flex'>EN
+        <p className='font-extrabold text-base flex '>EN
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
     </svg>
