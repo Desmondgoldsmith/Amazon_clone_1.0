5e657d69221f531fad6fd00568af90e4d24c95e6
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-28 10:50:39 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -48,12 +48,19 @@ function Header() {
        </div>
        {/* right section */}
       <div className='text-white text-xs flex space-x-2 items-center mx-6 whitespace-nowrap'>
-      <div>
+      
+            {/* language section */}
+      <div className='flex mr-2s'>
         <Image src='https://etc.usf.edu/clipart/72500/72519/72519_28_us_c_lg.gif'
           width={40}
           height={10}
           objectFit='contain'
         />
+        <p className='font-extrabold text-base flex'>EN
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+    </svg>
+        </p>
       </div>
       <div className='hover:border border border-transparent p-1 hover:border-solid hover:border-white cursor-pointer'>
         <p>Hello,Desmond Goldsmith</p>
