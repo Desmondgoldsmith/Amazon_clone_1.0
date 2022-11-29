dd91ba0994a6210ac1f42d5c13b071600bc5e41f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-29 12:20:12 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -262,7 +262,7 @@ function Header() {
 {/* second nav */}
       <div className='bg-amazon_blue-light h-8 text-white'>
       <div>
-        <div className='flex items-center space-x-2'>
+        <div className='flex items-center space-x-2 px-3 py-1'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
         <path fill-rule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
       </svg>
