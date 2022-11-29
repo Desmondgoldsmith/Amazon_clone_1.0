dfc1019e22e3e5fead8d03b74fb7d475ac2da726
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-29 12:27:22 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -264,7 +264,7 @@ function Header() {
       <div>
         <div className='flex items-center space-x-2 px-3 py-1'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-        <path fill-rule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+        <path fill-rule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
       </svg>
       <p>All</p>
         </div>
@@ -276,7 +276,9 @@ function Header() {
           <p>Sell</p>
         </div>
       </div>
-      <div></div>
+      <div>
+        <p>Disability Customer Support Turn up the cheer, shop gifts now</p>
+      </div>
       </div>
     </header>
   )
