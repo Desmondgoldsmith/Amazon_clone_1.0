bca34cf827dbcfe4c895e3d77150ac3f2965195c
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-29 13:29:17 +0000

    Amazon Clone Init

--- Src/Components/Sidenav.js
+++ Src/Components/Sidenav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sidenav() {
   return (
-    <div className='overflow-hidden max-w-xs w-4/5 h-full fixed z-50 top-0 left-0 bg-gray-500 origin-left'>Sidenav</div>
+    <div className='overflow-hidden max-w-xs w-4/5 h-full fixed z-50 top-0 left-0 bg-gray-400 origin-left'>Sidenav</div>
   )
 }
 
--- Src/pages/index.js
+++ Src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../Components/Header'
+import Sidenav from '../Components/Sidenav'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
       {/* header section */}
       <Header />
+      <Sidenav/>
     </div>
   )
 }
