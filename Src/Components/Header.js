ed831d89b6427b1f7a9e21d5d091d97a79c4641a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-29 12:53:37 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -260,13 +260,13 @@ function Header() {
       </div>
 
 {/* second nav */}
-      <div className='bg-amazon_blue-light h-8 text-white justify-between'>
+      <div className='bg-amazon_blue-light h-9 text-white flex items-center justify-between'>
       <div className='flex items-center'>
-        <div className='flex items-center space-x-2 px-3 py-1'>
+        <div className='flex items-center space-x-2 px-2 py-1 p-0.5 hover:border border-white'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
         <path fillRule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
       </svg>
-      <p className='font-extrabold'>All</p>
+      <p className='font-extrabold '>All</p>
         </div>
         <div className='flex space-x-2 text-sm cursor-pointer'>
           <p className='p-1 hover:border border-white'><Link href = "#" >Todat's Deal</Link></p>
@@ -276,8 +276,8 @@ function Header() {
           <p className='p-1 hover:border border-white'><Link href = "#" >Sell</Link></p>
         </div>
       </div>
-      <div>
-        <p>Disability Customer Support Turn up the cheer, shop gifts now</p>
+      <div className='pr-2 p-1 hover:border border-white'>
+        <p className='text-sm'>Turn up the cheer, shop gifts now</p>
       </div>
       </div>
     </header>
