8c74840ccb648c674c33ab2e8f8ff2558d445520
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-29 12:37:49 +0000

    Amazon Clone Init

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -261,14 +261,14 @@ function Header() {
 
 {/* second nav */}
       <div className='bg-amazon_blue-light h-8 text-white justify-between'>
-      <div>
+      <div className='flex items-center'>
         <div className='flex items-center space-x-2 px-3 py-1'>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-        <path fill-rule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+        <path fillRule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
       </svg>
-      <p>All</p>
+      <p className='font-extrabold'>All</p>
         </div>
-        <div className='text-white flex'>
+        <div className='flex  '>
           <p>Todat's Deal</p>
           <p>Customer Service</p>
           <p>Registry</p>
