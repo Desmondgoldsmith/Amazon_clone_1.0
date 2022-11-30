af11678a5ac0e7805128998d4a70886a0c975b87
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 09:11:43 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Subnavcontent.js
+++ Src/Components/Subnavcontent.js
@@ -5,13 +5,17 @@ function Subnavcontent() {
     const [openNavContent,setOpenNavContent] = useState(false)
   return (
     <>
+    {openNavContent && (
     <div>
      <div className='overflow-hidden  overflow-y-auto scrollbar-hide  max-w-xs w-4/5 h-full fixed z-50 top-0 left-0 bg-[rgba(0,0,0,0.7)] text-white origin-left'>
         Subnavcontt
      </div>
     
     </div>
+    )}
+
     <Sidenav/>
+
     </>
   )
 }
