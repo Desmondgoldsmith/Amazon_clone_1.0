720e815e83123e6d94a1256a58e359639bdeac62
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 17:17:58 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Sidenav.js
+++ Src/Components/Sidenav.js
@@ -191,7 +191,7 @@ function Sidenav({openNavContent,setOpenNavContent}) {
     {/* close sidebar button */}
     <div className='z-50 top-0 fixed  mt-4 '>
     <button onClick={(e)=>closeSideBar(e)}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-20 h-10 text-white">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-20 h-10 text-black">
         <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
         </svg>
     </button>
