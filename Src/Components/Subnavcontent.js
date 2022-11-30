47094af8542b1fc864b2ae24ed58cec17cb2f794
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 09:09:57 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Subnavcontent.js
+++ Src/Components/Subnavcontent.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 function Subnavcontent() {
+    const [openNavContent,setOpenNavContent] = useState(false)
   return (
     <>
     <div>
