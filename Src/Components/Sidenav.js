d49efeba52ff6939f33a5689fd4fda595789577a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 12:19:36 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Sidenav.js
+++ Src/Components/Sidenav.js
@@ -70,7 +70,7 @@ function Sidenav({subSidebar}) {
             </p>
            </div>
            <div className='text-black pt-2 pl-5 pb-2 hover:bg-gray-200 cursor-pointer '>
-            <p className='text-sm flex items-center justify-between'>Play 
+            <p className='text-sm flex items-center justify-between'>Play Music
             </p>
            </div>
         
