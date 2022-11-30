0a9e5c833f4852cad7c0576af403e18884aa9456
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 15:57:11 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -143,7 +143,7 @@ function Header() {
       {/* sign-in [display only on small screens] */}
       <div className = 'lg:hidden sm:flex p-1 hover:border hover:border-white'>
         <p className='flex items-center font-extrabold'>Sign in 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 font-extrabold">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
   </svg>
 </p>
