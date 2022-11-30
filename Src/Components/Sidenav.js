649b65ea06e1cb9076cee4320406bfc140521db4
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 11:09:59 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Sidenav.js
+++ Src/Components/Sidenav.js
@@ -13,6 +13,13 @@ function Sidenav() {
         setOpenNav(false)
         setCloseBtn(false)
     }
+    
+    const closeSubSideBar = (e) => {
+        e.preventDefault()
+        setOpenNavContent(false)
+        setCloseBtn(false)
+    }
+
 
     const openSubNav = (e) => {
         e.preventDefault()
@@ -31,8 +38,18 @@ function Sidenav() {
      <div className='overflow-hidden  overflow-y-auto scrollbar-hide  max-w-xs w-4/5 h-full fixed z-50 top-0 left-0 bg-blue-300 text-white origin-left'>
         Subnavcontt
      </div>
+     
+     {/* close sidebar button */}
+    <div className='z-50 top-0 fixed  mt-4 '>
+    <button onClick={(e)=>closeSubSideBar(e)}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-20 h-10 text-white">
+        <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+        </svg>
+    </button>
+    </div>
+
       {/* blurring the screen when side nav shows */}
-    <div className='top-0 left-0 w-[1200px] h-[178vh] z-20 bg-[rgba(0,0,0,0.7)]' onClick={(e)=>closeSideBar(e)}>
+    <div className='top-0 left-0 w-[1200px] h-[178vh] z-20 bg-[rgba(0,0,0,0.7)]' onClick={(e)=>closeSubSideBar(e)}>
     </div>
     
     </div>
@@ -185,7 +202,7 @@ function Sidenav() {
 
 
          </div>
-    {/* // translate-x-[-420px] */}
+    {/* close sidebar button */}
     <div className='z-50 top-0 fixed  mt-4 '>
     <button onClick={(e)=>closeSideBar(e)}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-20 h-10 text-white">
