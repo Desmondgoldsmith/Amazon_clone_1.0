1e84c639c644032fb7d9f94e19cf6638a98de101
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 16:46:09 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Searchbar.js
+++ Src/Components/Searchbar.js
@@ -5,20 +5,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
 function Searchbar() {
   return (
     <>
-     <select className='hidden md:inline w-13 focus:outline-orange-400 text-center h-full text-xs bg-gray-300 text-gray-600 rounded-l-md' name="cars" id="cars">
-        <option value="volvo">All</option>
-        <option value="saab">Arts</option>
-        <option value="mercedes">Automotive</option>
-        <option value="audi">Baby</option>
-        <option value="audi">Books</option>
-        <option value="audi">Deals</option>
-        <option value="audi">Electronics</option>
-        <option value="audi">Luggage</option>
-        <option value="audi">Movie</option>
-        <option value="audi">Deals</option>
-        <option value="audi">Deals</option>
-        <option value="audi">Deals</option>
-      </select>
+    
          <input className='p-2 items-center w-6 h-full flex-grow  focus:outline-none' type="text" />
          <MagnifyingGlassIcon  className = 'h-12 p-4 font-extrabold '/>
     </>
