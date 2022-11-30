615c360283dd5bea5734aa0a8a3a0aac8df8d388
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-30 15:48:36 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -141,12 +141,12 @@ function Header() {
         </nav>
       </div>
       {/* sign-in [display only on small screens] */}
-      <div className = ''>
-        <p>Sign in </p>
+      <div className = 'hidden sm:flex'>
+        <p className='flex items-center'>Sign in 
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
   </svg>
-
+</p>
       </div>
       <div className='hidden lg:block hover:border hoverable border border-transparent p-1 hover:border-solid hover:border-white cursor-pointer group relative '>
         <p>Hello,Desmond Goldsmith</p>
