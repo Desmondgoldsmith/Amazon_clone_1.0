e9d1a5bb31c52b6abc6290817e13f1164a96a7c7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-01 16:44:32 +0000

    Working on the Main Page 🚀

--- Src/Components/Banner.js
+++ Src/Components/Banner.js
@@ -27,9 +27,6 @@ function Banner() {
         <img loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/41Mz2pC1MzL-_SX1500_.jpg' alt='banner3'/>
      </div>
      <div>
-        <img loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/41Mz2pC1MzL-_SX1500_.jpg' alt='banner3'/>
-     </div>
-     <div>
         <img loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/61BvxKSpy3L-_SX3000_.jpg' alt='banner3'/>
      </div>
      <div>
