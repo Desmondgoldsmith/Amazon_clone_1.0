1139bd128c3c381f00114f208022112bdd70ae08
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-01 12:07:22 +0000

    Almost done with the Nav and SIdebar

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -305,7 +305,7 @@ function Header() {
       </div>
       {/* visible on small screens */}
       <div className='bg-amazon_blue-light border border-amazon_blue-light h-9 lg:hidden sm:flex md:hidden'>
-        <div className='flex flex-row  text-white ml-2 mr-2 hover:border border border-transparent p-1  cursor-pointer'>
+        <div className='flex flex-row space-x-2 text-white ml-2 mr-2 hover:border border border-transparent p-1  cursor-pointer'>
                 <p className='flex flex-row pl-3'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
