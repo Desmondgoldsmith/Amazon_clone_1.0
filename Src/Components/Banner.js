854d6e4ca409a2bf0260cc66007905df599d5478
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-01 17:18:52 +0000

    Working on the Main Page 🚀

--- Src/Components/Banner.js
+++ Src/Components/Banner.js
@@ -6,6 +6,7 @@ import { Carousel } from 'react-responsive-carousel'
 function Banner() {
   return (
     <div className='relative'>
+    <div className='absolute w-full  h-32  bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20 '/>
       <Carousel
        autoPlay
        infiniteLoop
