67ad7e188dc5230947fe382a3de188b7f51b37a2
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-01 16:35:38 +0000

    Working on the Main Page 🚀

--- Src/Components/Banner.js
+++ Src/Components/Banner.js
@@ -15,7 +15,9 @@ function Banner() {
      <div>
         <img loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/71dbxIcDioL-_SX3000_.jpg' alt='banner1'/>
      </div>
-     <div></div>
+     <div>
+        <img loading='lazy' src='' alt='banner2'/>
+     </div>
      <div></div>
       </Carousel>
     </div>
