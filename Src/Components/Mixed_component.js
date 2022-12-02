7d3dce364ea63d1c5b40bd881e11d93ef86e7851
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 22:33:08 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import Image from 'next/image'
 
-function Mixed_component({header,productImage1, productImage2, productImage3, productImage4,productName }) {
+function Mixed_component({header,productImage1, productImage2, productImage3, productImage4,productName1,productName2,productName3,productName4 }) {
   return (
     <div className='bg-white p-6  w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
         <p className='font-extrabold whitespace-nowrap'>{header}</p>
       <div className='flex space-x-2'>
-         <Image src={productImage1} 
+         <div className='block'>
+           <Image src={productImage1} 
         width={0}
         height={0}
         className = "w-[120px] h-[120px] " 
       />
-         <Image src={productImage2} 
-        width={0}
-        height={0}
-        className = "w-[120px] h-[120px] " 
-      />
+      <p className='font-bold'>{productName1}</p> 
+    </div>
+         <div className='block'>
+            <Image src={productImage2} 
+            width={0}
+            height={0}
+            className = "w-[120px] h-[120px] " 
+      /> 
+            <p className='font-bold'>{productName2}</p> 
+
+         </div>
+        
        </div>
        <div className='flex space-x-2'>
            <Image src={productImage3} 
