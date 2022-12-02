358c861c22ee7763466977d81f2cbe718b98059b
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 21:51:15 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -6,20 +6,20 @@ function Mixed_component({header,productImage1, productImage2, productImage3, pr
     <div className='bg-white p-10   w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
         <p className='font-extrabold whitespace-nowrap'>{header}</p>
       <Image src={productImage1} 
-        width={200}
-        height={200} 
+        width={120}
+        height={120} 
       />
          <Image src={productImage2} 
-        width={200}
-        height={200} 
+        width={120}
+        height={120} 
       />
          <Image src={productImage3} 
-        width={200}
-        height={200} 
+        width={120}
+        height={120} 
       />
          <Image src={productImage4} 
-        width={200}
-        height={200} 
+        width={120}
+        height={120} 
       />
     </div>
   )
