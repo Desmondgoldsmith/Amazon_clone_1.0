2fc3de98428dd841a22e136895688bd863ef0248
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 21:48:28 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Mixed_component({header,productImage1, productImage2, productImage3, productImage4,productName }) {
   return (
     <div className='bg-white p-10   w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
+        <p className='font-extrabold whitespace-nowrap'>{header}</p>
       <Image src={productImage1} 
         width={200}
         height={200} 
