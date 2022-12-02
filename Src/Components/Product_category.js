97670941fb920b451b8e9981e303a56d1e334849
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 13:00:32 +0000

    Working on the Main Page 🚀

--- Src/Components/Product_category.js
+++ Src/Components/Product_category.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Product_category() {
   return (
-    <div className='bg-white p-10 border-2 border-red-600  w-[400px]'>
+    <div className='bg-white p-10 border-2 border-red-600  w-[400px] z-30 h-[300px]'>
       <p>The individual category goes here</p>
     </div>
   )
