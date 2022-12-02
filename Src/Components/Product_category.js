980b2a5da7d7f160f10f3a46c2ddfd521324f8e7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 13:46:46 +0000

    Working on the Main Page 🚀

--- Src/Components/Product_category.js
+++ Src/Components/Product_category.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Product_category({header, productImage, productImage1, productImage2, productImage3, productImage4, productName}) {
   return (
     <div className='bg-white p-10   w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
       <p className='font-extrabold whitespace-nowrap'>{header}</p>
-      <Image src={} alt='product images'/>
+      <Image src={productImage} alt='product images'
+        width={30}
+        height={30} 
+      />
     </div>
   )
 }
