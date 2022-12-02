6cde8e25d7b1a2e4a47f061ad88fe28a691b51a0
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 13:34:22 +0000

    Working on the Main Page 🚀

--- Src/Components/Product_category.js
+++ Src/Components/Product_category.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Product_category({header}) {
   return (
     <div className='bg-white p-10   w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
-      <p className=''>{header}</p>
+      <p className='top-0 font-extrabold whitespace-nowrap'>{header}</p>
     </div>
   )
 }
