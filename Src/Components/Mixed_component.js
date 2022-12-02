7f1407a7a84092152a42d2ac40b7b6ecc0ae1040
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 23:07:58 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -4,42 +4,44 @@ import Image from 'next/image'
 function Mixed_component({header,productImage1, productImage2, productImage3, productImage4,productName1,productName2,productName3,productName4 }) {
   return (
     <div className='bg-white p-6  w-[400px] z-30 lg:h-[350px] md:h-[350px] sm:h-[50]'>
-        <p className='font-extrabold whitespace-nowrap'>{header}</p>
-      <div className='flex space-x-2'>
+        <p className='font-extrabold text-xl whitespace-nowrap'>{header}</p>
+      <div className='flex space-x-1'>
          <div className='block'>
         <Image src={productImage1} 
-        width={0}
-        height={0}
-        className = "w-[120px] h-[120px] " 
+        width={180}
+        height={400}
+        className = "w-[150px] h-[120px]  " 
       />
-      <p className='font-bold'>{productName1}</p> 
+      <p className='font-bold '>{productName1}</p> 
     </div>
          <div className='block'>
             <Image src={productImage2} 
-            width={0}
-            height={0}
-            className = "w-[120px] h-[120px] " 
+            width={180}
+            height={400}
+            className = "w-[150px] h-[120px]  " 
          /> 
             <p className='font-bold'>{productName2}</p> 
          </div>
        </div>
 
-       <div className='flex space-x-2'>
+       <div className='flex space-x-1'>
 
        <div className='block'>
         <Image src={productImage3} 
-        width={0}
-        height={0}
-        className = "w-[120px] h-[120px] " 
+         width={180}
+        height={400}
+        className = "w-[150px] h-[120px]  " 
+
       />
         <p className='font-bold'>{productName3}</p> 
        </div>
 
        <div className='block'>
          <Image src={productImage4} 
-        width={0}
-        height={0}
-        className = "w-[120px] h-[120px] " 
+        width={180}
+        height={400}
+        className = "w-[150px] h-[120px]  " 
+
       />
         <p className='font-bold'>{productName4}</p> 
        </div>
