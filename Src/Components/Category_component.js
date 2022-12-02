72453b32cf588d40601b09d7beb2cfcdc327e2a6
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 13:24:23 +0000

    Working on the Main Page 🚀

--- Src/Components/Category_component.js
+++ Src/Components/Category_component.js
@@ -3,7 +3,7 @@ import Product_category from './Product_category'
 
 function Category_component() {
   return (
-    <div className='flex space-x-3 ml-2 mr-2 border-2 border-green-600 -mt-[30px] lg:mt-[-200px] md:-mt-52'>
+    <div className='flex space-x-3 ml-2 mr-2 border-2 border-green-600 -mt-[30px] lg:mt-[-200px] md:-mt-52  overflow-x-scroll scrollbar-hide'>
       <Product_category/>
       <Product_category/>
       <Product_category/>
