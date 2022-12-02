0ae9525a1df235f1d32d9f415f5aa3169dcfdf5d
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 21:52:04 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -5,7 +5,8 @@ function Mixed_component({header,productImage1, productImage2, productImage3, pr
   return (
     <div className='bg-white p-10   w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
         <p className='font-extrabold whitespace-nowrap'>{header}</p>
-      <Image src={productImage1} 
+      <div className='flex'>
+         <Image src={productImage1} 
         width={120}
         height={120} 
       />
@@ -21,6 +22,8 @@ function Mixed_component({header,productImage1, productImage2, productImage3, pr
         width={120}
         height={120} 
       />
+      </div>
+     
     </div>
   )
 }
