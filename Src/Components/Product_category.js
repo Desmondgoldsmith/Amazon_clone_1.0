cdf1efb4dd360b911f53d3d5f3628c87decb1cf8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 12:30:33 +0000

    Working on the Main Page 🚀

---
+++ Src/Components/Product_category.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Product_category() {
+  return (
+    <div>
+      <p>The individual category goes here</p>
+    </div>
+  )
+}
+
+export default Product_category
