91c43e24180beac539126c53c1c8d70749038588
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 13:40:26 +0000

    Working on the Main Page 🚀

--- Src/Components/Category_component.js
+++ Src/Components/Category_component.js
@@ -4,10 +4,10 @@ import Product_category from './Product_category'
 function Category_component() {
   return (
     <div className='flex space-x-3 ml-2 mr-2 -mt-[30px] lg:mt-[-200px] md:-mt-52  overflow-x-scroll scrollbar-hide'>
-      <Product_category header = "Gaming Accessories"/>
-      <Product_category header = "Dresses"/>
-      <Product_category header = "Shop by category"/>
-      <Product_category header = "Health & Personal Care"/>
+      <Product_category header = "Gaming Accessories" productImage = "" productImage1 = "" productImage2 = "" productImage3 = "" productImage4 = ""  productName = "" />
+      <Product_category header = "Dresses" productImage = ""  productName = ""/>
+      <Product_category header = "Shop by category" productImage = ""  productName = ""/>
+      <Product_category header = "Health & Personal Care" productImage = ""  productName = ""/>
     </div>
   )
 }
