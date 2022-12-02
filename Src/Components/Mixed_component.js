349f79ff4edf032c69d3ba0dea41920f496574e8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-02 22:03:12 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 function Mixed_component({header,productImage1, productImage2, productImage3, productImage4,productName }) {
   return (
-    <div className='bg-white p-10   w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
+    <div className='bg-white p-6  w-[400px] z-30 lg:h-[300px] md:h-[300px] sm:h-[50]'>
         <p className='font-extrabold whitespace-nowrap'>{header}</p>
-      <div className='flex'>
+      <div className='flex space-x-2'>
          <Image src={productImage1} 
         width={120}
         height={120} 
@@ -15,7 +15,7 @@ function Mixed_component({header,productImage1, productImage2, productImage3, pr
         height={120} 
       />
        </div>
-       <div className='flex'>
+       <div className='flex space-x-2'>
            <Image src={productImage3} 
         width={120}
         height={120} 
