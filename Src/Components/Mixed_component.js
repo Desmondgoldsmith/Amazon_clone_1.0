90fe2603c9655f40c21ca9802fe48c741f300de2
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-03 16:12:47 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-function Mixed_component({link,header,productImage1, productImage2, productImage3, productImage4,productName1,productName2,productName3,productName4 }) {
-  return (
+function Mixed_component({background_color,link,header,productImage1, productImage2, productImage3, productImage4,productName1,productName2,productName3,productName4 }) {
+    return (
     <div className='bg-white p-6  w-[400px] z-30 lg:h-[400px] md:h-[400px] sm:h-[50]'>
         <p className='font-extrabold text-xl whitespace-nowrap mb-2 '>{header}</p>
       <div className='flex space-x-1 mb-2'>
-         <div className='block'>
+         <div className={`block`}>
         <Image src={productImage1} 
         width={180}
         height={400}
