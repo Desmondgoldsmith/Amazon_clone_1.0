fb952acd79551cdcf459ca654ab54646827bc4c3
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-03 16:43:58 +0000

    Working on the Main Page 🚀

---
+++ Src/Components/Mobileview_Category.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mobileview_Category() {
+  return (
+    <div className='bg-black p-1 w-[200px] z-30 h-[250px]'>
+       <h1>I'm a billonaire !</h1>
+    </div>
+  )
+}
+
+export default Mobileview_Category
