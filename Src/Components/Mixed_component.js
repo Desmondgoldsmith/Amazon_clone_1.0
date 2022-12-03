6cab873d409b8a74b82fcbb3e3657fbac735b51a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-03 23:38:43 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -7,7 +7,7 @@ function Mixed_component({background_color,link,header,productImage1, productIma
     <div className='bg-white p-6  w-[400px] z-30 h-[400px]'>
         <p className='font-extrabold text-xl mb-2 '>{header}</p>
       <div className='flex space-x-1 mb-2 '>
-         <div className={`block whitespace-nowrap`}>
+         <div className={`block`}>
         <Image src={productImage1} 
         width={180}
         height={400}
@@ -19,7 +19,7 @@ function Mixed_component({background_color,link,header,productImage1, productIma
             <Image src={productImage2} 
             width={180}
             height={400}
-            className = "w-[150px] h-[120px]  whitespace-nowrap" 
+            className = "w-[150px] h-[120px] " 
          /> 
             <p className='font-bold text-sm'>{productName2}</p> 
          </div>
@@ -27,17 +27,17 @@ function Mixed_component({background_color,link,header,productImage1, productIma
 
        <div className='flex space-x-1 mb-2'>
 
-       <div className='block whitespace-nowrap'>
+       <div className='block'>
         <Image src={productImage3} 
          width={180}
         height={400}
-        className = "w-[150px] h-[120px]  whitespace-nowrap " 
+        className = "w-[150px] h-[120px]  " 
 
       />
         <p className='font-bold text-sm'>{productName3}</p> 
        </div>
 
-       <div className='block whitespace-nowrap'>
+       <div className='block'>
          <Image src={productImage4} 
         width={180}
         height={400}
