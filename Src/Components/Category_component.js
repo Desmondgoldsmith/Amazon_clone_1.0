f3ca33164cb1725f4c518378d7425d665b170b7d
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-03 23:46:58 +0000

    Working on the Main Page 🚀

--- Src/Components/Category_component.js
+++ Src/Components/Category_component.js
@@ -11,7 +11,7 @@ function Category_component() {
       <Product_category link = "shop now" header = "Get fit at home" productImage = "https://images-na.ssl-images-amazon.com/images/G/01/events/GFAH/GWDesktop_SingleImageCard_fitathome_2x._SY608_CB434924743_.jpg" />
       <Product_category link = "shop now" header = "Easy returns" productImage = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Returns_2x._SY608_CB432774709_.jpg" />
       <Mixed_component link = "shop now" background_color = "red-600" header = "Shop by category" productImage1 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/computer.jpg" productImage2 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/PS4.jpg" productImage3 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/Baby240X-_SY170_CB468850909_.jpg" productImage4 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/Toys240X-_SY170_CB468851693_.jpg"  productName1 = "Computers & Acc" productName2 = "Video Games" productName3 = "Baby" productName4= "Toy & Games"/>
-      <div className='p-1  w-[400px] z-30 lg:h-[400px] md:h-[400px] sm:h-[50] space-y-7 flex-shrink'>
+      <div className='p-1  w-[350px] z-30 lg:h-[400px] md:h-[400px] sm:h-[50] space-y-7 flex-shrink'>
         <div className='bg-white p-6'>
             <p className='font-extrabold text-xl'>Sign in for the best experience</p>
             <button className='p-1 bg-yellow-300 w-[280px] rounded-md hover:bg-yellow-500'>sign in securely</button>
@@ -22,7 +22,7 @@ function Category_component() {
             width={180}
             height={300}
             alt="Fuji"
-            className = "w-[350px] h-[230px]"
+            className = "w-[350px] h-[260px]"
             />
         </div>
       </div>
