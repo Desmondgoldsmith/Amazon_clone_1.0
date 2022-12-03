952a0d9625759ce70562e5da0391950a5f4b7665
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-03 17:41:57 +0000

    Working on the Main Page 🚀

--- Src/Components/Mixed_component.js
+++ Src/Components/Mixed_component.js
@@ -6,8 +6,8 @@ function Mixed_component({background_color,link,header,productImage1, productIma
     return (
     <div className='bg-white p-6  w-[400px] z-30 h-[400px]'>
         <p className='font-extrabold text-xl whitespace-nowrap mb-2 '>{header}</p>
-      <div className='flex space-x-1 mb-2'>
-         <div className={`block`}>
+      <div className='flex space-x-1 mb-2 '>
+         <div className={`block whitespace-nowrap`}>
         <Image src={productImage1} 
         width={180}
         height={400}
@@ -19,7 +19,7 @@ function Mixed_component({background_color,link,header,productImage1, productIma
             <Image src={productImage2} 
             width={180}
             height={400}
-            className = "w-[150px] h-[120px]  " 
+            className = "w-[150px] h-[120px]  whitespace-nowrap" 
          /> 
             <p className='font-bold text-sm'>{productName2}</p> 
          </div>
@@ -27,17 +27,17 @@ function Mixed_component({background_color,link,header,productImage1, productIma
 
        <div className='flex space-x-1 mb-2'>
 
-       <div className='block'>
+       <div className='block whitespace-nowrap'>
         <Image src={productImage3} 
          width={180}
         height={400}
-        className = "w-[150px] h-[120px]  " 
+        className = "w-[150px] h-[120px]  whitespace-nowrap " 
 
       />
         <p className='font-bold text-sm'>{productName3}</p> 
        </div>
 
-       <div className='block'>
+       <div className='block whitespace-nowrap'>
          <Image src={productImage4} 
         width={180}
         height={400}
--- Src/Components/Product_category.js
+++ Src/Components/Product_category.js
@@ -4,12 +4,12 @@ import Link from 'next/link'
 
 function Product_category({link,header, productImage}) {
   return (
-    <div className='bg-white p-6  w-[400px] z-30 h-[400px]  mb-2'>
+    <div className='bg-white p-6  w-[400px] z-30 h-[400px]  mb-2 whitespace-nowrap'>
       <p className='font-extrabold whitespace-nowrap text-xl'>{header}</p>
       <Image src={productImage} alt='product images'
         width={180}
         height={400}
-        className = 'h-[310px] w-[350px]'
+        className = 'h-[310px] w-[350px] whitespace-nowrap'
       />
         <Link className='text-blue-500 hover:text-red-600 hover:underline' href="#">{link}</Link>
     </div>
