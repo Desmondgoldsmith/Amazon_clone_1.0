3903c701789ca0551a9f2a6815be18f2ed4ecb6e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-03 23:28:38 +0000

    Working on the Main Page 🚀

--- Src/Components/Mobileview_Category.js
+++ Src/Components/Mobileview_Category.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Mobileview_Category({title}) {
   return (
-    <div className='bg-white p-1 w-[150px] z-30 h-[200px]'>
+    <div className='bg-white p-1 w-[500px] z-30 h-[200px] whitespace-nowrap border-2 border-red-600'>
        {/* <p>{title}</p> */}
-       <p>Big heads</p>
+       {/* <p>Big heads</p> */}
     </div>
   )
 }
