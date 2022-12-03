4ef3ca5a4f3c99a6f40f2281dadb197672659ae2
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-03 14:10:54 +0000

    Working on the Main Page 🚀

--- Src/Components/Category_component.js
+++ Src/Components/Category_component.js
@@ -4,7 +4,7 @@ import Product_category from './Product_category'
 
 function Category_component() {
   return (
-    <div className='ml-2 mr-2 -mt-[30px] lg:mt-[-320px] md:-mt-[-320px]  overflow-x-scroll scrollbar-hide '>
+    <div className='space-y-3 ml-2 mr-2 -mt-[30px] lg:mt-[-320px] md:-mt-[-320px]  overflow-x-scroll scrollbar-hide '>
       <div className='z-30 space-x-3  flex'>
       <Product_category link = "shop now" header = "Get fit at home" productImage = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/Dresses.jpg" />
       <Product_category link = "shop now" header = "Easy returns" productImage = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/Dresses.jpg" />
