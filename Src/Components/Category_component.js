009a2febfa8bda90b40a2f5f2c43d92c12aaa2c0
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-04 01:00:45 +0000

    Working on the Main Page 🚀

--- Src/Components/Category_component.js
+++ Src/Components/Category_component.js
@@ -13,7 +13,7 @@ function Category_component() {
       <Mixed_component link = "shop now" background_color = "red-600" header = "Shop by category" productImage1 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/computer.jpg" productImage2 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/PS4.jpg" productImage3 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/Baby240X-_SY170_CB468850909_.jpg" productImage4 = "https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/Images/Toys240X-_SY170_CB468851693_.jpg"  productName1 = "Computers & Acc" productName2 = "Video Games" productName3 = "Baby" productName4= "Toy & Games"/>
       <div className='p-1  w-[350px] z-30 lg:h-[400px] md:h-[400px] sm:h-[50] space-y-7 '>
         <div className='bg-white p-6'>
-            <p className='font-extrabold text-xl'>Sign in for the best experience</p>
+            <p className='font-extrabold text-xl whitespace-normal'>Sign in for the best experience</p>
             <button className='p-1 bg-yellow-300 w-[280px] rounded-md hover:bg-yellow-500'>sign in securely</button>
         </div>
         <div >
@@ -22,7 +22,7 @@ function Category_component() {
             width={180}
             height={300}
             alt="Fuji"
-            className = "w-[325px] h-[260px]"
+            className = "w-[325px] h-[230px]"
             />
         </div>
       </div>
