527f38547a839a3a2e79b33063560c52cf65feee
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-04 13:10:07 +0000

    Working on the Main Page 🚀

--- Src/Components/Mobileview_Category.js
+++ Src/Components/Mobileview_Category.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
-function Mobileview_Category({title}) {
+function Mobileview_Category({title,image}) {
   return (
     <div className='bg-white p-1 w-[150px] shrink-0 z-30 h-[200px]   border-2 border-red-600'>
        <p>{title}</p>
-       
+       <Image 
+       src={image}
+       alt={title}
+       width={150}
+       height={200}  
+       />
     </div>
   )
 }
