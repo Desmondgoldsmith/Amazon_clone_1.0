6f5a822780c525a504c82de833141d0003fc9a25
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-04 16:44:45 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_products.js
+++ Src/Components/Top_products.js
@@ -3,7 +3,7 @@ import Shopgift from './Shopgift'
 
 function Top_products() {
   return (
-    <div className='border-2 border-red-600 h-[250px]'>
+    <div className='border-2 border-red-600 h-[250px] ml-8 mr-11 mb-20 bg-white'>
    <Shopgift/>
     </div>
   )
