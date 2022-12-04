a3fa496dbf0ae3a5966e6abe333f5140447fc925
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-04 16:57:54 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_products.js
+++ Src/Components/Top_products.js
@@ -3,15 +3,23 @@ import Shopgift from './Shopgift'
 
 function Top_products() {
   return (
-    <div className='border-2 border-red-600 h-[250px] ml-8 mr-11 mb-20 bg-white flex overflow-x-scroll scrollbar-hide'>
-   <Shopgift/>
-   <Shopgift/>
-   <Shopgift/>
-   <Shopgift/>
-   <Shopgift/>
-   <Shopgift/>
-   <Shopgift/>
-   <Shopgift/>
+    <div className='border-2  border-red-600 h-[250px] ml-8 mr-11 mb-20 bg-white '>
+      <p className='pl-8'>This is the Title</p>
+    <div className='flex  overflow-x-scroll scrollbar-hide'>
+   
+    <Carousel>
+      <Shopgift/>
+      <Shopgift/>
+      <Shopgift/>
+      <Shopgift/>
+      <Shopgift/>
+      <Shopgift/>
+      <Shopgift/>
+      <Shopgift/>
+    </Carousel>
+    
+    </div>
+   
     </div>
   )
 }
