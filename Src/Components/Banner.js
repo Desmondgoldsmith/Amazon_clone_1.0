a266c283f0f9b33888c534a4db0d07dbe450c11b
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-04 14:17:17 +0000

    Working on the Main Page 🚀

--- Src/Components/Banner.js
+++ Src/Components/Banner.js
@@ -19,28 +19,28 @@ function Banner() {
       >
        
      <div>
-        <img className='border-2 border-red-600  h-[280px]' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/71dbxIcDioL-_SX3000_.jpg' alt='banner1'/>
+        <img className='border-2 border-red-600  h-[280px] lg:h-full ' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/71dbxIcDioL-_SX3000_.jpg' alt='banner1'/>
      </div>
      <div>
-        <img  className='border-2 border-red-600  h-[280px]' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/41J4WL-j4OL-_SX1500_.jpg' alt='banner2'/>
+        <img  className='border-2 border-red-600  h-[280px] lg:h-full' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/41J4WL-j4OL-_SX1500_.jpg' alt='banner2'/>
      </div>
      <div>
-        <img  className='border-2 border-red-600  h-[280px]' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/51x7E2BAaRL-_SX1500_.jpg' alt='banner2'/>
+        <img  className='border-2 border-red-600  h-[280px] lg:h-full' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/51x7E2BAaRL-_SX1500_.jpg' alt='banner2'/>
      </div>
      <div>
-        <img className='border-2 border-red-600  h-[280px]'  loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/41Mz2pC1MzL-_SX1500_.jpg' alt='banner3'/>
+        <img className='border-2 border-red-600  h-[280px] lg:h-full'  loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/41Mz2pC1MzL-_SX1500_.jpg' alt='banner3'/>
      </div>
      <div>
-        <img  className='border-2 border-red-600  h-[280px]' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/71tIrZqybrL-_SX3000_.jpg' alt='banner3'/>
+        <img  className='border-2 border-red-600  h-[280px] lg:h-full' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/71tIrZqybrL-_SX3000_.jpg' alt='banner3'/>
      </div>
      <div>
-        <img  className='border-2 border-red-600  h-[280px]' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/61BvxKSpy3L-_SX3000_.jpg' alt='banner3'/>
+        <img  className='border-2 border-red-600  h-[280px] lg:h-full' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/61BvxKSpy3L-_SX3000_.jpg' alt='banner3'/>
      </div>
      <div>
-        <img  className='border-2 border-red-600  h-[280px]'loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/71qid7QFWJL-_SX3000_.jpg' alt='banner3'/>
+        <img  className='border-2 border-red-600  h-[280px] lg:h-full'loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/71qid7QFWJL-_SX3000_.jpg' alt='banner3'/>
      </div>
      <div>
-        <img className='border-2 border-red-600  h-[280px]' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/61shpu9mvML-_SX3000_.jpg' alt='banner3'/>
+        <img className='border-2 border-red-600  h-[280px] lg:h-full' loading='lazy' src='https://raw.githubusercontent.com/Desmondgoldsmith/Amazon_clone_1.0/main/public/61shpu9mvML-_SX3000_.jpg' alt='banner3'/>
      </div>
     
       </Carousel>
