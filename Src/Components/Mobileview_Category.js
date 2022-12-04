324c7e26bd9899dd4f447dfba8f389dd73d685cd
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-04 13:15:31 +0000

    Working on the Main Page 🚀

--- Src/Components/Mobileview_Category.js
+++ Src/Components/Mobileview_Category.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Mobileview_Category({title,image}) {
   return (
     <div className='bg-white p-1 w-[150px] shrink-0 z-30 h-[200px]   border-2 border-red-600'>
-       <p>{title}</p>
+       <p className='font-bold'>{title}</p>
        <Image 
        src={image}
        alt={title}
