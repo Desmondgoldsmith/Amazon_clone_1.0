25a5c5b3468cb1410f7b81d7d869070dc603b1fa
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-04 16:47:52 +0000

    Working on the Main Page 🚀

--- Src/Components/Shopgift.js
+++ Src/Components/Shopgift.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Shopgift() {
   return (
-    <div className='w-[200px] border-2 border-green-700 truncate-normal bg-white'>
+    <div>
+     <p>This is the Title</p>
+     <div className='w-[200px] h-[200px] border-2 border-green-700 truncate-normal bg-white ml-4 mr-4'>
        <h1 className=''>This component will contain all the DATA  to be passed to the Top_product component for rendering</h1>
+
+     </div>
      </div>
   )
 }
