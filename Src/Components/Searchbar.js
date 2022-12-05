33cef848712f5e5e6b5f26b135472cac5bf451f8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-05 14:24:00 +0000

    Working on the Main Page 🚀

--- Src/Components/Searchbar.js
+++ Src/Components/Searchbar.js
@@ -5,7 +5,6 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
 function Searchbar() {
   return (
     <>
-    
          <input className='p-2 items-center w-6 h-full flex-grow  focus:outline-none' placeholder='search on Amazon' type="text" />
          <MagnifyingGlassIcon  className = 'h-12 p-4 font-extrabold '/>
     </>
