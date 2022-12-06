b2094a3a7513afe6ca10d71f8b49ae6dbaa0c87a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-06 15:13:32 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_products.js
+++ Src/Components/Top_products.js
@@ -40,7 +40,7 @@ function Top_products() {
       <Shopgift/>
       <Shopgift/>    
     </div>
-    <div className = "text-dark p-1 hover:rounded-full hover:bg-gray-500 hover:text-white cursor-pointer">
+    <div className = "text-dark p-1 hover:rounded-full hover:bg-gray-500 hover:text-white cursor-pointer" onClick={rightScroll}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clipRule="evenodd" />
   </svg>
