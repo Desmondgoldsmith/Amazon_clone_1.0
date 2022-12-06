0d6723b8c372adf84c11dd9d81ec9e763acf25c6
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-06 22:26:14 +0000

    Working on the Main Page 🚀

--- Src/Components/Shopgift.js
+++ Src/Components/Shopgift.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Shopgift() {
+function Shopgift({productImage}) {
   return (
     <div className='ml-4 mr-4 p-3'>
      <div className='w-[200px] h-[200px]  truncate-normal bg-white '>
