cd4587e1d21b84cae2147b1c83bef849ae0a36f7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-06 14:49:55 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_products.js
+++ Src/Components/Top_products.js
@@ -3,9 +3,10 @@ import Shopgift from './Shopgift'
 
 function Top_products() {
   return (
-    <div className='border-2 flex items-center  border-red-600 h-[250px] ml-8 mr-11 mb-20 bg-white '> 
+    <div className='border-2   border-red-600 h-[250px] ml-8 mr-11 mb-20 bg-white '> 
      <p className='pl-8'>This is the Title</p>
-   <div className = "text-dark">
+   <div className='flex items-center'>
+     <div className = "text-dark">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
   </svg>
@@ -22,6 +23,8 @@ function Top_products() {
       <Shopgift/>
       <Shopgift/>    
     </div>
+   </div>
+   
       </div>
    
   )
