2149bbfca36a7928e575823359a1275b8630d5ae
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-06 23:04:37 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_products.js
+++ Src/Components/Top_products.js
@@ -35,8 +35,8 @@ function Top_products() {
       <Shopgift productImage = "https://m.media-amazon.com/images/I/51Mj5OtsMvL._AC_SY200_.jpg"/>
       <Shopgift productImage = "https://m.media-amazon.com/images/I/51XZJwsgnmL._AC_SY200_.jpg"/>
       <Shopgift productImage = "https://m.media-amazon.com/images/I/61bPiEPsl4L._AC_SY400_.jpg"/>
-      <Shopgift productImage = ""/>
-      <Shopgift productImage = ""/>
+      <Shopgift productImage = "https://m.media-amazon.com/images/I/61dYy8ZB8LL._AC_SY200_.jpg"/>
+      <Shopgift productImage = "https://m.media-amazon.com/images/I/61hXNZ7p0-S._AC_SY200_.jpg"/>
       <Shopgift productImage = ""/>
       <Shopgift productImage = ""/>    
     </div>
