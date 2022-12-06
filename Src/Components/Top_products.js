7624c513c64306fca129867995152b0e555c7384
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-06 14:20:17 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_products.js
+++ Src/Components/Top_products.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Shopgift from './Shopgift'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel'
 function Top_products() {
   return (
     <div className='border-2  border-red-600 h-[250px] ml-8 mr-11 mb-20 bg-white '>  <p className='pl-8'>This is the Title</p>
