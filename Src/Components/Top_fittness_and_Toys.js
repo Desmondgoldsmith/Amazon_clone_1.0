69bb959a81ae50ee8d1e992798c5f356e66fc8a7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-07 21:30:50 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_fittness_and_Toys.js
+++ Src/Components/Top_fittness_and_Toys.js
@@ -4,10 +4,10 @@ import Fitness_and_Toys from './Fitness_and_Toys'
 function Top_fittness_and_Toys() {
   return (
     <div className='hidden lg:flex md:flex z-30 space-x-5 ml-8 mr-8 whitespace-nowrap overflow-x-scroll scrollbar-hide '>
-      <Fitness_and_Toys  title = "" productImage=""/>
-      <Fitness_and_Toys title = "" productImage=""/>
-      <Fitness_and_Toys title = "" productImage=""/>
-      <Fitness_and_Toys title = "" productImage=""/>
+      <Fitness_and_Toys  title = "For your Fitness Needs" productImage=""/>
+      <Fitness_and_Toys title = "Shop activity trackers and smartwatches" productImage=""/>
+      <Fitness_and_Toys title = "New arrivals in Toys" productImage=""/>
+      <Fitness_and_Toys title = "Gifts for every stocking" productImage=""/>
     </div>
   )
 }
