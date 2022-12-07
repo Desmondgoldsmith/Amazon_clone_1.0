50336b21cc5401c2d7e56676f5987046e3b12ae2
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-07 13:46:28 +0000

    Working on the Main Page 🚀

--- Src/Components/Shopgift.js
+++ Src/Components/Shopgift.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function Shopgift({productImage}) {
   return (
-    <div className='ml-4 mr-4 p-3 border border-green-800'>
+    <div className='ml-2 mr-2 p-1 border border-green-800'>
      <div className='w-[150px] h-[200px] border border-red-600 truncate-normal bg-white '>
        <Image src = {productImage} 
         width={150}
