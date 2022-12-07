0759937c85ced344844141d62a7051a5eb9decab
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-07 14:07:37 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_Apparels.js
+++ Src/Components/Top_Apparels.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Top_Apparels() {
   return (
-    <div>Top_Apparels</div>
+    <div className='w-[200px] h-[200px] border-2 border-red-600 truncate-normal'>
+    Top_Apparels
+    </div>
   )
 }
 
