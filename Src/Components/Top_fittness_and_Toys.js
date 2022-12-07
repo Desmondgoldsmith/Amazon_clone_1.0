5635927e5ff5cd00f5eb31a81f6ea8277b67e9f5
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-07 21:47:44 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_fittness_and_Toys.js
+++ Src/Components/Top_fittness_and_Toys.js
@@ -7,7 +7,7 @@ function Top_fittness_and_Toys() {
       <Fitness_and_Toys  title = "For your Fitness Needs" productImage="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Fitness_1X._SY304_CB639748186_.jpg"/>
       <Fitness_and_Toys title = "Shop activity trackers and smartwatches" productImage="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_SmartWatch_1X._SY304_CB639922137_.jpg"/>
       <Fitness_and_Toys title = "New arrivals in Toys" productImage="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Toys_1X._SY304_CB639759658_.jpg"/>
-      <Fitness_and_Toys title = "Gifts for every stocking" productImage=""/>
+      <Fitness_and_Toys title = "Gifts for every stocking" productImage="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2022/HOL22/StockingStuffers/GW/QuadCard/DT/HOL22_SS_GW_QuadCard_ShopAll_DT_1x._SY116_CB606650457_.jpg"/>
     </div>
   )
 }
