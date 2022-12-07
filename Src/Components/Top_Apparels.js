8610dc0ff7e7b0006bbba5bcaa1bbeb49ae84535
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-07 14:11:11 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_Apparels.js
+++ Src/Components/Top_Apparels.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Image from 'next/image'
 
-function Top_Apparels() {
+function Top_Apparels({productImage}) {
   return (
     <div className='ml-2 mr-2 p-1'>
      <div className='w-[150px] h-[200px]  truncate-normal bg-white '>
