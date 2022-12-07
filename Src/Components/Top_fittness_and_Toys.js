09b2cf5d9d2793b01cd54d782dcfc53a11619aba
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-07 21:22:51 +0000

    Working on the Main Page 🚀

--- Src/Components/Top_fittness_and_Toys.js
+++ Src/Components/Top_fittness_and_Toys.js
@@ -3,7 +3,7 @@ import Fitness_and_Toys from './Fitness_and_Toys'
 
 function Top_fittness_and_Toys() {
   return (
-    <div>
+    <div className='hidden lg:flex md:flex z-30 space-x-5 whitespace-nowrap overflow-x-scroll scrollbar-hide '>
       <Fitness_and_Toys/>
     </div>
   )
