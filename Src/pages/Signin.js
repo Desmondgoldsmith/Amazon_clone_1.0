4cb96dd55589816b6b7ea4ecd01bc828eeab1e77
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-08 23:06:18 +0000

    Working on the Sign-in page

--- Src/pages/Signin.js
+++ Src/pages/Signin.js
@@ -8,8 +8,8 @@ function Signin() {
         <Image 
         src = "http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png"
         alt = "logo"
-        width ={100}
-        height = {100} 
+        width ={150}
+        height = {150} 
         />
       </div>
     </div>
