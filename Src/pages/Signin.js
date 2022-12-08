dd84ec320084ede75b843c3c7d8e2898396b1648
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-08 23:04:32 +0000

    Working on the Sign-in page

--- Src/pages/Signin.js
+++ Src/pages/Signin.js
@@ -4,12 +4,12 @@ import Image from 'next/image'
 function Signin() {
   return (
     <div className='bg-[rgb(255,255,255)]'>
-      <div>
+      <div className='flex flex-col justify-center items-center border-2 border-red-600 '>
         <Image 
         src = "http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png"
         alt = "logo"
         width ={100}
-        height = {100}
+        height = {100} 
         />
       </div>
     </div>
