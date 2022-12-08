4ece8aef4f5f27d0a6752d64a257f40964dcf78e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-08 23:06:35 +0000

    Working on the Sign-in page

--- Src/pages/Signin.js
+++ Src/pages/Signin.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Signin() {
   return (
     <div className='bg-[rgb(255,255,255)]'>
-      <div className='flex flex-col justify-center items-center border-2 border-red-600 '>
+      <div className='flex flex-col justify-center items-center'>
         <Image 
         src = "http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png"
         alt = "logo"
