69dcb1f89020bb2b0e6bf88ee8f0ab5db94d37a1
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-09 23:57:02 +0000

    Working on the Sign-in page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBErKNr-8EJJZSXvHyfMo56BjBV9nSqi08",
+    authDomain: "clone-9bb9b.firebaseapp.com",
+    projectId: "clone-9bb9b",
+    storageBucket: "clone-9bb9b.appspot.com",
+    messagingSenderId: "665544799111",
+    appId: "1:665544799111:web:8a8693b03e4a1d74ec5922",
+    measurementId: "G-N21V1PSJSE"
+  };
