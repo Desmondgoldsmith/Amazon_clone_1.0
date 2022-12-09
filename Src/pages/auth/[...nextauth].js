10b9f3783212c81c32a89425855b25cce3bd01ef
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-09 22:40:47 +0000

    Working on the Sign-in page

--- Src/pages/auth/[...nextauth].js
+++ Src/pages/auth/[...nextauth].js
@@ -7,22 +7,9 @@ import EmailProvider from 'next-auth/providers/email'
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
-    }),
-    // Passwordless / email sign in
-    EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: 'NextAuth.js <no-reply@example.com>'
     }),
   ]
 })
