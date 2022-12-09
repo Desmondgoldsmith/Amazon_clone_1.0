980836ae9f510c0312247b7f6a1f3a6698d38c95
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-09 00:17:32 +0000

    Working on the Sign-in page

--- Src/pages/Signin.js
+++ Src/pages/Signin.js
@@ -3,38 +3,41 @@ import Image from 'next/image'
 
 function Signin() {
   return (
-    <div className='bg-[rgb(255,255,255)] grid justify-items-stretch'>
+    <div className='bg-[rgb(255,255,255)] grid justify-items-stretch -mt-[30px]'>
     {/* logo */}
       <div className='justify-self-center'>
         <Image 
         src = "http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png"
         alt = "logo"
         width ={150}
-        height = {150} 
+        height = {100} 
         />
       </div>
       {/* sign-in form */}
-    <div className='border w-[400px] justify-self-center pl-3 pr-3'>
+    <div className='border w-[380px] justify-self-center pl-2 pr-2 -mt-[20px]'>
         <h1 className='text-[30px]'>Create account</h1>
         <form className='mt-3'> 
         <label for = "name" className='font-bold grid mb-2'>Your name</label>
-        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='First and last name' />
+        <input className='rounded-md border w-[360px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='First and last name' />
 
         <label for = "name" className='font-bold grid mb-2 mt-2'>Mobile number or email</label>
-        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' />
+        <input className='rounded-md border w-[360px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' />
         
         <label for = "name" className='font-bold grid mb-2 mt-2'>Password</label>
-        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='At least 6 characters' />
+        <input className='rounded-md border w-[360px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='At least 6 characters' />
          <p className='text-xs'>Passwords must be at least 6 characters long</p>
 
         <label for = "name" className='font-bold grid mb-2 mt-2'>Re-enter password</label>
-        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name'/>
+        <input className='rounded-md border w-[360px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name'/>
         
-        <button className='bg-[rgb(240,197,97)] hover:bg-yellow-500 w-[370px] h-[35px] mt-3 rounded-md'>Continue</button>
-        <button className='bg-white w-[370px] h-[35px] hover:bg-black hover:text-white mt-3 rounded-md'>Sign-in with Google</button>
+        <button className='bg-[rgb(240,197,97)] hover:bg-yellow-500 w-[360px] h-[35px] mt-3 rounded-md'>Continue</button>
+        <button className='bg-white w-[360px] h-[35px] hover:bg-black hover:text-white mt-3 rounded-md'>Sign-in with Google</button>
          <p className='text-xs mt-3'>By creating an account, you agree to Amazon's <span className='text-blue-500'>Conditions of Use</span>  and <span className='text-blue-500'>Privacy Notice</span>.</p>
          
          </form>
+         <hr className='mt-4 w-[250px]'/>
+         <p >Already have an account? Sign in</p>
+         <p>Buying for work? Create a free business account</p>
       </div>
       
 
