68b49a421519b0ff7b7f98f3c44ed7e91aba0224
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-09 00:06:43 +0000

    Working on the Sign-in page

--- Src/pages/Signin.js
+++ Src/pages/Signin.js
@@ -14,23 +14,25 @@ function Signin() {
         />
       </div>
       {/* sign-in form */}
-    <div className='border w-[450px] justify-self-center pl-3 pr-3'>
+    <div className='border w-[400px] justify-self-center pl-3 pr-3'>
         <h1 className='text-[30px]'>Create account</h1>
         <form className='mt-6'> 
         <label for = "name" className='font-bold grid mb-2'>Your name</label>
-        <input className='rounded-md border w-[400px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='First and last name' />
+        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='First and last name' />
 
         <label for = "name" className='font-bold grid mb-2 mt-2'>Mobile number or email</label>
-        <input className='rounded-md border w-[400px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' />
+        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' />
         
         <label for = "name" className='font-bold grid mb-2 mt-2'>Password</label>
-        <input className='rounded-md border w-[400px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='At least 6 characters' />
+        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='At least 6 characters' />
          <p className='text-xs'>Passwords must be at least 6 characters long</p>
 
         <label for = "name" className='font-bold grid mb-2 mt-2'>Re-enter password</label>
-        <input className='rounded-md border w-[400px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name'/>
+        <input className='rounded-md border w-[370px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name'/>
         
-        <button className='bg-[rgb(240,197,97)] w-[400px] h-[40px] mt-3'>Continue</button>
+        <button className='bg-[rgb(240,197,97)] w-[370px] h-[35px] mt-3 rounded-md'>Continue</button>
+         <p className='text-xs mt-3'>By creating an account, you agree to Amazon's <span className='text-blue-500'>Conditions of Use</span>  and <span className='text-blue-500'>Privacy Notice</span>.</p>
+         
          </form>
       </div>
       
