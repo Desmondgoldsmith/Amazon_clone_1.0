fa862f4c71746656be6f1dc26271bdabbef9663f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-10 10:26:11 +0000

    Working on the sign-in page with Next-Auth

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -17,7 +17,7 @@ function Header() {
        <div className = 'text-white lg:hidden sm:flex hover:border border-white'>
        <Sidenav />
        </div>
-       <div className='mt-2 items-center flex-grow sm:flex-grow-0 hover:border border border-transparent p-1 hover:border-solid hover:border-white cursor-pointer'>
+       <div onClick={()=>router('/')} className='mt-2 items-center flex-grow sm:flex-grow-0 hover:border border border-transparent p-1 hover:border-solid hover:border-white cursor-pointer'>
          <Image src = 'https://pngimg.com/uploads/amazon/amazon_PNG25.png'
           width={100}
           height={20}
--- Src/pages/Signin.js
+++ Src/pages/Signin.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 function Signin() {
   return (
