5ff983e2181aa22a51607086198e439ff4cf994b
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-10 00:26:53 +0000

    Working on the Sign-in page

--- Src/pages/auth/[...nextauth].js
+++ Src/pages/auth/[...nextauth].js
@@ -1,8 +1,5 @@
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
 
 export default NextAuth({
   providers: [
