466f3be2fd5b5ea5087e119e7778000a750ef888
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-10 09:35:54 +0000

    Working on the sign-in page with Next-Auth

--- Src/pages/_app.js
+++ Src/pages/_app.js
@@ -1,16 +1,16 @@
 import { Provider } from 'react-redux'
 import { store } from '../App/Store'
 import '../styles/globals.css'
-import {Provider as NextAuthProvier} from "next-auth/react"
+import { SessionProvider } from "next-auth/react"
 
 
 function MyApp({ Component, pageProps }) {
   return( 
-   <NextAuthProvider session = {pageProps.session} >
+   <SessionProvider session = {pageProps.session} >
 <Provider store={store}>
       <Component {...pageProps} />
    </Provider>
-   </NextAuthProvider>
+   </SessionProvider>
   
 )}
 
