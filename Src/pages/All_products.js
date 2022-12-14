7ab0a2f70950ebdf17f472e748a65b8d7752312f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 20:06:53 +0000

    working on the Cart page

--- Src/pages/All_products.js
+++ Src/pages/All_products.js
@@ -29,7 +29,7 @@ function All_products() {
     </svg>
          </Link> </p>
         <p className = "cursor-pointer text-[13px] "><Link href="/#">
-        <div className = "flex-row border-2 border-red-600">
+        <div className = "flex border-2 border-red-600">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
             </svg>
