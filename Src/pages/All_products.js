54ded56feb6e34c2bf896503e9a823db436c01b9
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 15:31:09 +0000

    Working on the cart page

--- Src/pages/All_products.js
+++ Src/pages/All_products.js
@@ -3,11 +3,12 @@ import Header from '../Components/Header'
 function All_products() {
   return (
     <div>
-    <div >
+    <div className = "flex" >
     <div className = "border border-red-600 w-[1250px]">
       <Header/>
     </div>
-    <div className = "border border-green-600 w-[]">
+    <div className = "border border-green-600 w-[180px]">
+    <h1>this is the cart section</h1>
     </div>
  
     </div>
