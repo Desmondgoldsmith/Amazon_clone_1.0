d43b7387b97a8589485d1a153678a48fbc716188
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 15:44:13 +0000

    Working on the cart page

--- Src/pages/All_products.js
+++ Src/pages/All_products.js
@@ -8,11 +8,11 @@ function All_products() {
       <Header/>
 
       {/* page content  */}
-      <div className = "flex">
+      <div className = "flex mt-5">
        <div className="w-[300px] border-2 border-red-600">
         <h1>Left component</h1>
        </div>
-       <div className = "w-[900px] border-2 border-blue-600">
+       <div className = "w-[1000px] border-2 border-blue-600">
         <h1>Right component</h1>
        </div>
       </div>
