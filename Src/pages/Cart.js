44806742b8f5c2864d493c7bf3848dd3d2a933e2
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 13:39:26 +0000

    Working on the cart page

--- Src/pages/Cart.js
+++ Src/pages/Cart.js
@@ -25,9 +25,9 @@ function Cart() {
               <p className='text-green-600 text-[13px]'>In Stock</p>
               <p className = "text-[13px]">Gift options not available.<span className = "text-blue-500">Learn more</span> </p>
               <p className='text-[13px]'><span className='font-bold'>Style:</span> Book</p>
-              <div>
+              <div className = "flex">
                 <select className='bg-gray-200 outline-none border border-gray-200 p-2 rounded-md' name="" id="">
-                  <option value="">Qty:1</option>
+                  <option value="Qty:1">Qty:1</option>
                   <option value="">1</option>
                   <option value="">2</option>
                   <option value="">3</option>
@@ -39,6 +39,10 @@ function Cart() {
                   <option value="">9</option>
                   <option value="">10+</option>
                 </select>
+
+                <p>| <span className  = "text-blue-500 hover:text-underline">delete</span> |</p>
+                <p>| <span className  = "text-blue-500 hover:text-underline">save for later</span> |</p>
+                <p>| <span className  = "text-blue-500 hover:text-underline">compare with similar items</span> |</p>
               </div>
             </div>
               
