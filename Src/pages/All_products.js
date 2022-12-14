eba10516eb4412a19d22d1b5663423e3cab9def0
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 15:57:20 +0000

    Working on the cart page

--- Src/pages/All_products.js
+++ Src/pages/All_products.js
@@ -12,14 +12,20 @@ function All_products() {
       <div className = "flex mt-5">
        <div className="w-[300px] border-2 border-red-600 p-1">
          <p className='text-[16px] font-extrabold'>Departments</p>
-         <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#">Women's Fashion</Link> </p>
-         <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#">Women's Shoes</Link> </p>
-        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#"> Women's Clothing</Link></p>
-        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#">Home & Kitchen</Link> </p>
-        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#">Men's Fashion</Link> </p>
-        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#">Boys' Fashion</Link> </p>
-        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#">Girls' Jewelry</Link> </p>
-        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="#">Bath & Bathing Accessories</Link> </p>
+         <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#">Women's Fashion</Link> </p>
+         <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#">Women's Shoes</Link> </p>
+        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#"> Women's Clothing</Link></p>
+        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#">Home & Kitchen</Link> </p>
+        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#">Men's Fashion</Link> </p>
+        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#">Boys' Fashion</Link> </p>
+        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#">Girls' Jewelry</Link> </p>
+        <p className = "cursor-pointer hover:underline hover:text-orange-400 text-[13px]"><Link href="/#">Bath & Bathing Accessories</Link> </p>
+       
+        <p className='text-[16px] font-extrabold'>Customer Reviews</p>
+         <p className = "cursor-pointer text-[13px]"><Link href="/#">Women's Fashion</Link> </p>
+         <p className = "cursor-pointer text-[13px]"><Link href="/#">Women's Shoes</Link> </p>
+        <p className = "cursor-pointer text-[13px]"><Link href="/#"> Women's Clothing</Link></p>
+        
        </div>
        <div className = "w-[1000px] border-2 border-blue-600">
         <h1>Right component</h1>
