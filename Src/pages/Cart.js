24b70e1dd8a767efb8bd39d8e0172591547bb6a9
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 12:59:33 +0000

    Working on the cart page

--- Src/pages/Cart.js
+++ Src/pages/Cart.js
@@ -10,8 +10,19 @@ function Cart() {
       {/* cart product section */}
         <div className = "w-[1150px] mr-5 bg-white p-3">
             <p className = "text-[25px]">Shopping Cart</p>
-            <p className = "text-right text-[14px] font-bold text-gray-300">price</p>
+            <p className = "text-right text-[13px] font-bold text-gray-500">price</p>
             <hr/>
+            {/* // cart items */}
+            <div className = " mt-4">
+            <div>
+              <img src="https://m.media-amazon.com/images/I/51LS7arqYHL._AC_SY200_.jpg"
+               alt="cart image" 
+               className = "w-[200px]"
+               />
+            </div>
+            <div></div>
+              
+            </div>
         </div>
         {/* total price section */}
         <div className = "w-[240px] bg-white ">
