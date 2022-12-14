fdc1f9a68e41b8d2ff07f8227a33770465fb2b03
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 12:37:13 +0000

    Working on the cart page

--- Src/pages/Cart.js
+++ Src/pages/Cart.js
@@ -8,12 +8,12 @@ function Cart() {
 
       <div className = "flex mt-[20px] ml-4 mr-4">
       {/* cart product section */}
-        <div className = "w-[1150px] mr-5 bg-white border-2 border-black">
+        <div className = "w-[1150px] mr-5 bg-white ">
             <p>Shopping Cart</p>
             <hr/>
         </div>
         {/* total price section */}
-        <div className = "w-[240px] bg-white border-2 border-red-600">
+        <div className = "w-[240px] bg-white ">
           <p>Subtotal</p>
         </div>
       </div>
