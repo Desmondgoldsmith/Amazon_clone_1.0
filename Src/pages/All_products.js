fe51bb21a6954ce38e30d7a7eb1a36a9c713f873
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 15:34:19 +0000

    Working on the cart page

--- Src/pages/All_products.js
+++ Src/pages/All_products.js
@@ -4,10 +4,10 @@ function All_products() {
   return (
     <div>
     <div className = "flex" >
-    <div className = "border border-red-600 w-[1250px]">
+    <div className = "border border-red-600 w-[1280px]">
       <Header/>
     </div>
-    <div className = "border border-green-600 w-[180px]">
+    <div className = "border border-green-600 w-[160px]">
     <h1>this is the cart section</h1>
     </div>
  
