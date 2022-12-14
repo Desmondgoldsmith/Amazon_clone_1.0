e2e9ebadf20d5d986ea291ff39bbeec67c18de51
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 12:31:55 +0000

    Working on the cart page

--- Src/pages/Cart.js
+++ Src/pages/Cart.js
@@ -3,17 +3,17 @@ import Header from '../Components/Header'
 
 function Cart() {
   return (
-    <div>
+    <div className = "bg-gray-300">
       <Header />
 
-      <div className = "flex">
+      <div className = "flex mt-[25px]">
       {/* cart product section */}
-        <div className = "w-[1200px] bg-white border-2 border-black">
+        <div className = "w-[1150px] bg-white border-2 border-black">
             <p>Shopping Cart</p>
             <hr/>
         </div>
         {/* total price section */}
-        <div className = "w-[300px] bg-white border-2 border-red-600">
+        <div className = "w-[240px] bg-white border-2 border-red-600">
           <p>Subtotal</p>
         </div>
       </div>
