816f73fd4a8d4ab9b825a0216d393a1b9f9eb372
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 15:18:09 +0000

    Working on the cart page

--- Src/App/Store.js
+++ Src/App/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../Slices/Cartslice";
-
+// globak store
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
---
+++ Src/pages/All_products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Components/Header'
+function All_products() {
+  return (
+    <div>
+     <Header/>
+    </div>
+  )
+}
+
+export default All_products
