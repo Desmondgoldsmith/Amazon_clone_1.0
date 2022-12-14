e963dcc88967d34ed71d59b16fb7ef88ebc0e724
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-14 12:45:35 +0000

    Working on the cart page

--- Src/pages/Cart.js
+++ Src/pages/Cart.js
@@ -10,7 +10,7 @@ function Cart() {
       {/* cart product section */}
         <div className = "w-[1150px] mr-5 bg-white p-3">
             <p className = "text-[25px]">Shopping Cart</p>
-            <p className = "text-left text-[12px]">price</p>
+            <p className = "text-right text-[12px]">price</p>
             <hr/>
         </div>
         {/* total price section */}
