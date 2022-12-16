2f2919522d5dd0ddfff450562c03e3ec4233d286
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-16 19:27:24 +0000

    Working on the Cart Page

--- Src/pages/Signin.js
+++ Src/pages/Signin.js
@@ -50,6 +50,17 @@ function Signin() {
 }
 // using server side auth
 export async function getServerSideProps(context){
-  
+  const session = await getSession(context)
+  if(!session){
+    return{
+      redirect : {destination : '/'}
+    }
+  }
+
+  return{
+    props:{
+      session
+    }
+  }
 }
 export default Signin
