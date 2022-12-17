d556b788765d56da48c4d4d2ae03d859bd7348f3
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 18:51:00 +0000

    Working on the cart page

--- Src/Components/EmptyCart.js
+++ Src/Components/EmptyCart.js
@@ -8,8 +8,8 @@ function EmptyCart() {
     const router = useRouter()
   return (
     <div>
-      <div className ="block lg:flex md:flex   bg-white mx-0 my-2 md:mx-0 md:my-5  lg:mx-0 lg:my-5 p:2 lg:p-3 md:p-3  space-x-0 md:space-x-20 lg:space-x-20 w-full md:w-full lg:w-[1150px]">
-            <div className=' flex flex-col justify-center items-center lg:flex lg:flex-none lg:justify-self-auto lg:items-auto md:flex md:flex-none md:justify-self-auto md:items-auto'>
+      <div className ="block lg:flex md:flex   bg-white mt-0 mb-2 my-2 md:mx-0 md:my-5  lg:mx-0 lg:my-5 p:2 lg:p-3 md:p-3  space-x-0 md:space-x-20 lg:space-x-20 w-full md:w-full lg:w-[1150px]">
+            <div className=' flex flex-col justify-center pt-3 items-center lg:flex lg:flex-none lg:justify-self-auto lg:items-auto md:flex md:flex-none md:justify-self-auto md:items-auto'>
              <Image
              src = "https://m.media-amazon.com/images/G/01/cart/empty/kettle-desaturated._CB445243794_.svg"
              width = {100}
@@ -19,12 +19,12 @@ function EmptyCart() {
             </div>
             <div className = "pl-2 pr-4 ">
             <p className  = 'text-[23px] font-bold pt-10 w-full lg:w-none md:w-none text-center'>Your Amazon Cart is empty</p>
-            <p className ="text-[12px] text-center lg:text-justify md:text-justify cursor-pointer  text-blue-500 hover:underline hover:text-red-500">Shop today's deals</p>
+            <p className ="text-[12px] text-center mb-3 lg:text-justify md:text-justify cursor-pointer  text-blue-500 hover:underline hover:text-red-500">Shop today's deals</p>
             <button onClick={!session ? signIn : ()=>router.push("/All_products")} className = {`bg-yellow-400 p-2 rounded-md w-full mb-3 lg:w-[300px] md:w-[300px] pt-3`}>{!session ? "sign into your account" : "View products"}</button>
             </div>
             </div>
             <div className = " bg-white mx-0 mt-0 mb-4 md:mx-0 md:my-5 lg:ml-0 lg:mr-0 lg:my-5 p-3 w-full md:w-full lg:w-[1150px]">
-            <button onClick={()=>router.push("/All_products")} className = {`bg-yellow-400 p-2 rounded-md w-full mb-3 lg:w-[300px] md:w-[300px] pt-3`}>Shop now !</button>
+            <button onClick={()=>router.push("/All_products")} className = {`inline-block md:hidden lg:hidden bg-yellow-400 p-2 rounded-md w-full mb-3 lg:w-[300px] md:w-[300px] pt-3`}>Shop now !</button>
 
             </div>
 
