9eb93a213d3b2d4ed78c278ada359de652f3d321
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 16:29:59 +0000

    Working on the cart page

--- Src/Components/EmptyCart.js
+++ Src/Components/EmptyCart.js
@@ -8,7 +8,7 @@ function EmptyCart() {
     const router = useRouter()
   return (
     <div>
-      <div className ="block lg:flex md:flex border-2 border-red-600 bg-white mx-2 my-2 md:mx-5 md:my-5 lg:mx-5 lg:my-5 p:1 lg:p-3 md:p-3  space-x-0 md:space-x-20 lg:space-x-20 w-full md:w-[1150px] lg:w-[1150px]">
+      <div className ="block lg:flex md:flex border-2 border-red-600 bg-white mx-0 my-2 md:mx-5 md:my-5 lg:mx-5 lg:my-5 p:2 lg:p-3 md:p-3  space-x-0 md:space-x-20 lg:space-x-20 w-full md:w-[1150px] lg:w-[1150px]">
             <div>
              <Image
              src = "https://m.media-amazon.com/images/G/01/cart/empty/kettle-desaturated._CB445243794_.svg"
@@ -17,8 +17,8 @@ function EmptyCart() {
              className = "w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] md:w-[300px] md:h-[300px]"
              />
             </div>
-            <div className = "pl-2 pr-4 border-2 border-blue-600">
-            <p className  = 'text-[23px] font-bold pt-10 border-2 border-red-600'>Your Amazon Cart is empty</p>
+            <div className = "pl-2 pr-4 ">
+            <p className  = 'text-[23px] font-bold pt-10 sm:w-full lg:w-none md:w:none text-center'>Your Amazon Cart is empty</p>
             <p className ="text-[12px] cursor-pointer text-blue-500 hover:underline hover:text-red-500">Shop today's deals</p>
             <button onClick={!session ? signIn : ()=>router.push("/All_products")} className = {`bg-yellow-400 p-2 rounded-md w-[300px] pt-3`}>{!session ? "sign into your account" : "View products"}</button>
             </div>
