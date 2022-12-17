6cef6d37f45f0a96af652e805096b5c5ecdb3968
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 23:30:06 +0000

    working on the Cart page

--- Src/Components/ProductFeed.js
+++ Src/Components/ProductFeed.js
@@ -10,7 +10,7 @@ function ProductFeed({products}) {
         <p className= "text-xs">Price and other details may vary based on product size and color.</p>
        <hr className='mt-2 mb-2'/>
         {/* dixplay products */}
-      <div className = "block md:block lg:flex  flex-wrap space-x-4 p-0 md:p-2 lg:p-2 ">
+      <div className = "block md:block lg:flex  flex-wrap space-y-4 md:space-y-4 lg:space-x-4 p-0 md:p-2 lg:p-2 ">
         {products.map(({title,price,description,category,image,id})=>(
         <ProductList title={title} price={price} description={description} category={category} image = {image} id={id} key = {id}/>
         ))}
