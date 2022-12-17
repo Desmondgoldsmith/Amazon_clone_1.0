618463a45bd5596afb02dadd909aa4278f4986fc
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 15:15:58 +0000

    Working on the cart page

--- Src/Components/EmptyCart.js
+++ Src/Components/EmptyCart.js
@@ -25,7 +25,7 @@ function EmptyCart() {
             </div>
             <div className = " mt-5 mb-5 mx-5 p-5"></div>
 
-            <p>The price and availability of items at Amazon.com are subject to change. The Cart is a temporary place to store a list of your items and reflects each item's most recent price. Shopping CartLearn more
+            <p className ="text-sm mb-5">The price and availability of items at Amazon.com are subject to change. The Cart is a temporary place to store a list of your items and reflects each item's most recent price. Shopping CartLearn more
               Do you have a gift card or promotional code? We'll ask you to enter your claim code when it's time to pay.</p>
     </div>
   )
