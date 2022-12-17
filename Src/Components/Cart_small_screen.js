0016872e9653dc28d1706d481d7d5a007b5a2254
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 20:58:31 +0000

    working on the Cart page

--- Src/Components/Cart_small_screen.js
+++ Src/Components/Cart_small_screen.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 function Cart_small_screen() {
   return (
-    <div>
+    <div className = "w-full">
+         <div className = "block md:hidden lg:hidden mt-[20px] ml-4 mr-4">
+           {/* total price section */}
+        {/* prevent user form proceeding to checkout if ! signed in  */}
         
+        <div className = "w-[240px] bg-white p-2 h-[120px]">
+        <p className='text-[18px]'>Subtotal(1 item): <span className='font-extrabold'>${sum} </span> </p>
+         <hr/>
+          <button disabled = {!session} className = {`w-[220px] ${!session ? `bg-gray-300 cursor-not-allowed` : `bg-yellow-300 hover:bg-yellow-500` }  p-2 rounded-lg `}>{!session ? "sign-in first" :  "proceed to checkout"}</button>
+        </div>
+
+
+      {/* cart product section */}
+        <div className = "w-[1150px] mr-5 bg-white p-3 mb-5">
+           {/* {items.length == 0 ? } */}
+            <p className = "text-[25px]">Shopping Cart</p>
+            <p className = "text-right text-[13px] font-bold text-gray-500">price</p>
+            <hr/>
+            {items.map((item , i) => (
+            <CartItems title={item.title} price={item.price} description={item.description} category={item.category} image = {item.image} id={item.id} key = {i}/>
+             ))}
+              
+              {/* sub total */}
+              <p className='text-[20px] text-right'>Subtotal(1 item): <span className='font-extrabold'>${sum} </span> </p>
+        </div>
+       
+     
+      </div>
     </div>
   )
 }
