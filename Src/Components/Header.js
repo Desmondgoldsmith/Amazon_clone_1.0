4547b5d23d381edde2030974b5e5946eb39a4b8d
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 18:36:53 +0000

    Working on the cart page

--- Src/Components/Header.js
+++ Src/Components/Header.js
@@ -166,15 +166,11 @@ function Header() {
       </div>
       {/* sign-in [display only on small screens] */}
       <div className = 'lg:hidden sm:flex p-1 hover:border hover:border-white'>
-        <p onClick = {()=>router.push('/Signin')} className='flex items-center font-extrabold flex-wrap content-center border border-red-600 w-[60px]'>{session ? session.user.name : "Sign in"} 
-        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-      </svg> */}
-
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 font-extrabold">
+        <p onClick = {()=>router.push('/Signin')} className='flex items-center font-extrabold flex-wrap  line-clamp-2 truncate text-center w-[60px]'>
+        {session ? session.user.name : 
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 font-extrabold">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-  </svg>
-</p>
+  </svg> + "Sign in"} </p>
       </div>
       <div className='hidden lg:block hover:border hoverable border border-transparent p-1 hover:border-solid hover:border-white cursor-pointer group relative '>
         <p className = "font-bold truncate">{session ? `${session.user.name}` : "Sign-in"}</p>
