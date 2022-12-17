fadc73fe3ef7ceea2a7b3eac19111d4fd4f69588
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 17:09:37 +0000

    Working on the cart page

--- Src/Components/EmptyCart.js
+++ Src/Components/EmptyCart.js
@@ -9,12 +9,12 @@ function EmptyCart() {
   return (
     <div>
       <div className ="block lg:flex md:flex  bg-white mx-0 my-2 md:mx-5 md:my-5 lg:mx-5 lg:my-5 p:2 lg:p-3 md:p-3  space-x-0 md:space-x-20 lg:space-x-20 w-full md:w-[1150px] lg:w-[1150px]">
-            <div className='border-2 border-red-600'>
+            <div className='border-2 border-red-600 flex flex-col justify-center items-center lg:flex lg:flex-none lg:justify-self-auto lg:items-auto md:flex md:flex-none md:justify-self-auto md:items-auto'>
              <Image
              src = "https://m.media-amazon.com/images/G/01/cart/empty/kettle-desaturated._CB445243794_.svg"
              width = {100}
              height = {100}
-             className = "border-2 border-red-600 rounded-full lg:rounded-none md:rounded-none w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] md:w-[300px] md:h-[300px]"
+             className = " border-2 border-red-600 rounded-full lg:rounded-none md:rounded-none w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] md:w-[300px] md:h-[300px]"
              />
             </div>
             <div className = "pl-2 pr-4 ">
