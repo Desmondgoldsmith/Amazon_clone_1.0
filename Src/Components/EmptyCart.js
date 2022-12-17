9ed227474e9dbb76c07f231e71e2a464b0f311dd
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-17 16:09:09 +0000

    Working on the cart page

--- Src/Components/EmptyCart.js
+++ Src/Components/EmptyCart.js
@@ -8,7 +8,7 @@ function EmptyCart() {
     const router = useRouter()
   return (
     <div>
-      <div className ="block lg:flex md:flex bg-white ml-5 mr-5 my-5 p-3 space-x-20 w-full m-[1150px] lg:w-[1150px]">
+      <div className ="block lg:flex md:flex bg-white ml-5 mr-5 my-5 p-3 space-x-20 w-full md:w-[1150px] lg:w-[1150px]">
             <div>
              <Image
              src = "https://m.media-amazon.com/images/G/01/cart/empty/kettle-desaturated._CB445243794_.svg"
@@ -23,9 +23,9 @@ function EmptyCart() {
             <button onClick={!session ? signIn : ()=>router.push("/All_products")} className = {`bg-yellow-400 p-2 rounded-md w-[300px] pt-3`}>{!session ? "sign into your account" : "View products"}</button>
             </div>
             </div>
-            <div className = "bg-white mt-5 mb-5 mx-5 p-10 w-[1150px]"></div>
+            <div className = "bg-white mt-5 mb-5 mx-5 p-10 w-full md:w-[1150px] lg:w-[1150px]"></div>
 
-            <p className ="text-xs mb-5 w-[1100px] ml-5 mr-5">The price and availability of items at Amazon.com are subject to change. The Cart is a temporary place to store a list of your items and reflects each item's most recent price. Shopping Cart <span className = "text-blue-500 hover:text-red hover:underline cursor-pointer"> Learn more </span>
+            <p className ="text-xs mb-5 w-full md:w-[1150px] lg:w-[1150px] ml-5 mr-5">The price and availability of items at Amazon.com are subject to change. The Cart is a temporary place to store a list of your items and reflects each item's most recent price. Shopping Cart <span className = "text-blue-500 hover:text-red hover:underline cursor-pointer"> Learn more </span>
               Do you have a gift card or promotional code? We'll ask you to enter your claim code when it's time to pay.</p>
               <div className = " mt-5 mb-5 mx-5 p-3"></div>
     </div>
