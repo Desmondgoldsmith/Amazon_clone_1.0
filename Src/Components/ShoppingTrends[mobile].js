abe9269de9b9acc4479e45aa60976b1262dec77c
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-18 21:41:22 +0000

    Almost done

--- Src/Components/ShoppingTrends[mobile].js
+++ Src/Components/ShoppingTrends[mobile].js
@@ -10,7 +10,7 @@ function ShoppingTrends({Trends}) {
        <hr className='mt-2 mb-2'/>
         {/* dixplay products */}
       <div className = "block md:block lg:flex  flex-wrap space-y-4 md:space-y-4 lg:-space-y-0 lg:space-x-4 p-0 md:p-2 lg:p-2 ">
-        { Trends.map(({title,price,description,category,image,id})=>(
+        { Trends.slice(0,3).map(({title,price,description,category,image,id})=>(
         <ProductTrends title={title} price={price} description={description} category={category} image = {image} id={id} key = {id}/>
         ))}
       </div>
