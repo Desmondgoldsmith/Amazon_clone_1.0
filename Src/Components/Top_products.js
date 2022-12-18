8170ba7f4ab730a7111dcfb6d4cb40febb49643a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-18 14:46:55 +0000

    Working on Screen optimization

--- Src/Components/Top_products.js
+++ Src/Components/Top_products.js
@@ -107,15 +107,4 @@ function Top_products({Trends}) {
 }
 
 
-export async function getServerSideProps(context){
-  const Trends = await fetch("https://fakestoreapi.com/products").then(
-      (res) => res.json()
-  );
-  return{
-    props:{
-      Trends,
-  }
-  }
-}
-
 export default Top_products
