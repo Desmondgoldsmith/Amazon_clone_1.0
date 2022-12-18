41387cfacf5f80f79b4f194f1f9945540f165b24
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-18 21:06:33 +0000

    Almost done

--- Src/Components/ShoppingTrends[mobile].js
+++ Src/Components/ShoppingTrends[mobile].js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ProductTrends from './ProductTrends[mobile]'
 
-function ShoppingTrends({Trends}) {
+function ShoppingTrends({products}) {
      
   return (
     <div>
@@ -10,7 +10,7 @@ function ShoppingTrends({Trends}) {
        <hr className='mt-2 mb-2'/>
         {/* dixplay products */}
       <div className = "block md:block lg:flex  flex-wrap space-y-4 md:space-y-4 lg:-space-y-0 lg:space-x-4 p-0 md:p-2 lg:p-2 ">
-        { Trends.map(({title,price,description,category,image,id})=>(
+        { products.map(({title,price,description,category,image,id})=>(
         <ProductTrends title={title} price={price} description={description} category={category} image = {image} id={id} key = {id}/>
         ))}
       </div>
