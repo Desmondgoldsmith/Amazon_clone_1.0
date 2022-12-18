24562134546f55bd30d9020e59db5a5e3a0925b7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-18 22:33:33 +0000

    almost done

--- Src/Components/ProductTrends[mobile].js
+++ Src/Components/ProductTrends[mobile].js
@@ -5,7 +5,7 @@ function ProductTrends({title,price,image,id}) {
   return (
     <div>
         <div className='flex md:hidden lg:hidden space-x-2 w-full items-center'>
-         <div className='p-1 box-border w-[130px] h-[130px] w- border-2 border-red-600'>
+         <div className='p-1 '>
           <Image 
           src = {image}
           alt = "product images"
@@ -15,7 +15,7 @@ function ProductTrends({title,price,image,id}) {
           />
          </div>
           
-          <div className = "p-1 border-2 border-red-600">
+          <div className = "p-2 w-full">
               <p className = "block md:block lg:hidden text-[16px] font-semibold cursor-pointer truncate whitespace-normal  hover:overflow-visible hover:whitespace-normal hover:ease-in hover:duration-300">{title}</p>
         <p>$<span className = "font-extrabold text-[18px]">{price}</span></p>   
         <button onClick = {(e)=>addCart(e)} className ="p-2 w-[180px]  rounded-md bg-yellow-500 hover:bg-yellow-600">Add to cart</button>
