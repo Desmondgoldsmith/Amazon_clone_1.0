44883342d4b94dd879ad659f8555d956f8406568
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 14:01:48 +0000

    fixing some bugs

--- Src/Components/Product_category.js
+++ Src/Components/Product_category.js
@@ -3,17 +3,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 
-function Product_category({name,link,header, productImage}) {
+function Product_category({title,route,header, productImage}) {
   const router = useRouter()
   return (
     <div className='bg-white p-6  w-[325px] z-30 h-[400px]  mb-2 shrink-0'>
       <p className='font-extrabold whitespace-nowrap text-xl'>{header}</p>
-      <Image src={productImage} alt='product images'
+      <Image onClick = {route} src={productImage} alt='product images'
         width={180}
         height={400}
         className = 'h-[310px] w-[350px] whitespace-nowrap'
       />
-        <p className='text-blue-500 cursor-pointer hover:text-red-600 hover:underline' onClick = {link}>{name}</p>
+        <p className='text-blue-500 cursor-pointer hover:text-red-600 hover:underline' onClick = {route}>{title}</p>
     </div>
   )
 }
