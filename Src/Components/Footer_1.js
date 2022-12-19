c5129968b34d9bf1b3f2ab7d62424f0223286eba
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 16:24:00 +0000

    fixing some bugs

--- Src/Components/Footer_1.js
+++ Src/Components/Footer_1.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Footer_1() {
   return (
-    <div className = "border-2 text-white bg-amazon_blue-light pl-40 pr-40 mt-6 pt-6 pb-6 grid gap-10 grid-cols-4">
+    <div className = " text-white bg-amazon_blue-light pl-40 pr-40 mt-6 pt-6 pb-6 grid gap-10 grid-cols-4">
       <div  className = "border-2 border-red-600">
       <p className = "font-extrabold text-[20px]">Get to Know Us</p>
-       <p> Careers</p>
-       <p> Blog</p>
-       <p> About Amazon</p>
-       <p> Investor Relations</p>
-       <p> Amazon Devices</p>
-       <p> Amazon Science</p>
+       <p><Link href = "/#" > Careers</Link></p>
+       <p><Link href = "/#" > Blog</Link></p>
+       <p> <Link href = "/#" >About Amazon</Link></p>
+       <p> <Link href = "/#" >Investor Relations</Link></p>
+       <p><Link href = "/#" > Amazon Devices</Link></p>
+       <p><Link href = "/#" > Amazon Science</Link></p>
       </div>
       <div className = "border-2 border-red-600">Second one</div>
       <div className = "border-2 border-red-600">Third one</div>
