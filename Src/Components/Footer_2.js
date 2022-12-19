4e3030de3d8bf493b4722e8be4f50751c7786fff
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 18:13:58 +0000

    fixing some bugs

--- Src/Components/Footer_2.js
+++ Src/Components/Footer_2.js
@@ -3,57 +3,111 @@ import Link from 'next/link'
 
 function Footer_2() {
   return (
-    <div className = "p-10 h-[300px] text-white bg-amazon_blue">
+    <div className = "p-10  text-white bg-amazon_blue">
       <div className = "grid gap-6 grid-cols-7 grid-rows-4 ml-40 mr-40">
-      <div className = "border border-red-600">
+      <div className = "">
       <p className = "text-[12px] font-extrabold">Amazon Music</p>
         <p  className = "text-xs"><Link href = "/#">Stream millions</Link></p>
         <p className = "text-xs"><Link href = "/#">of songs</Link></p>	
       </div>
-      <div className = "border border-red-600">
+      <div className = "">
       <p className = "text-[12px] font-extrabold"> Amazon Advertising</p>
       <p className = "text-xs"><Link href = "/#">Find, attract, and</Link></p>
       <p className = "text-xs"><Link href = "/#">engage customers</Link></p>
       </div>
-      <div className = "border border-red-600">
+      <div className = "">
       <p className = "text-[12px] font-extrabold">Amazon Drive</p>
     <p className = "text-xs"><Link href = "/#">Cloud storage</Link></p>
     <p className = "text-xs"><Link href = "/#">from Amazon</Link></p>
       </div>
-      <div className = "border border-red-600">
+      <div className = "">
       <p className = "text-[12px] font-extrabold">6pm</p>
 <p className = "text-xs"><Link href = "/#">Score deals</Link></p>
 <p className = "text-xs"><Link href = "/#">on fashion brands</Link></p>
       </div>
-      <div className = "border border-red-600">
+      <div className = "">
       <p className = "text-[12px] font-extrabold">AbeBooks</p>
 <p className = "text-xs"><Link href = "/#">Books, art</Link></p>
 <p className = "text-xs"><Link href = "/#">& collectibles</Link></p>
       </div>
-      <div className = "border border-red-600">
+      <div className = "">
       <p className = "text-[12px] font-extrabold">ACX</p>
 <p className = "text-xs"><Link href = "/#">Audiobook Publishing</Link></p>
 <p className = "text-xs"><Link href = "/#">Made Easy</Link></p>
       </div>
-      <div className = "border border-red-600">
+      <div className = "">
       <p className = "text-[12px] font-extrabold">Sell on Amazon </p>
       <p className = "text-xs"><Link href = "/#">Start a Selling Account</Link></p>
       </div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
-      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">Amazon Music</p>
+        <p  className = "text-xs"><Link href = "/#">Stream millions</Link></p>
+        <p className = "text-xs"><Link href = "/#">of songs</Link></p>	
       </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold"> Amazon Advertising</p>
+      <p className = "text-xs"><Link href = "/#">Find, attract, and</Link></p>
+      <p className = "text-xs"><Link href = "/#">engage customers</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">Amazon Drive</p>
+    <p className = "text-xs"><Link href = "/#">Cloud storage</Link></p>
+    <p className = "text-xs"><Link href = "/#">from Amazon</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">6pm</p>
+<p className = "text-xs"><Link href = "/#">Score deals</Link></p>
+<p className = "text-xs"><Link href = "/#">on fashion brands</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">AbeBooks</p>
+<p className = "text-xs"><Link href = "/#">Books, art</Link></p>
+<p className = "text-xs"><Link href = "/#">& collectibles</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">ACX</p>
+<p className = "text-xs"><Link href = "/#">Audiobook Publishing</Link></p>
+<p className = "text-xs"><Link href = "/#">Made Easy</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">Sell on Amazon </p>
+      <p className = "text-xs"><Link href = "/#">Start a Selling Account</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">Amazon Music</p>
+        <p  className = "text-xs"><Link href = "/#">Stream millions</Link></p>
+        <p className = "text-xs"><Link href = "/#">of songs</Link></p>	
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold"> Amazon Advertising</p>
+      <p className = "text-xs"><Link href = "/#">Find, attract, and</Link></p>
+      <p className = "text-xs"><Link href = "/#">engage customers</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">Amazon Drive</p>
+    <p className = "text-xs"><Link href = "/#">Cloud storage</Link></p>
+    <p className = "text-xs"><Link href = "/#">from Amazon</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">6pm</p>
+<p className = "text-xs"><Link href = "/#">Score deals</Link></p>
+<p className = "text-xs"><Link href = "/#">on fashion brands</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">AbeBooks</p>
+<p className = "text-xs"><Link href = "/#">Books, art</Link></p>
+<p className = "text-xs"><Link href = "/#">& collectibles</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">ACX</p>
+<p className = "text-xs"><Link href = "/#">Audiobook Publishing</Link></p>
+<p className = "text-xs"><Link href = "/#">Made Easy</Link></p>
+      </div>
+      <div className = "">
+      <p className = "text-[12px] font-extrabold">Sell on Amazon </p>
+      <p className = "text-xs"><Link href = "/#">Start a Selling Account</Link></p>
+      </div>
+            </div>
     </div>
   )
 }
