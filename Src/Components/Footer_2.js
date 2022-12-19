04e7226d98d6cf4dd34ffad1ccf50d62dbdb63b9
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 17:51:00 +0000

    fixing some bugs

--- Src/Components/Footer_2.js
+++ Src/Components/Footer_2.js
@@ -2,8 +2,30 @@ import React from 'react'
 
 function Footer_2() {
   return (
-    <div className = "p-10 h-[300px] border-2 border-red-600">
-      <p>look</p>
+    <div className = "p-10 h-[300px] text-white bg-amazon_blue">
+      <div className = "grid gap-6 grid-cols-7 grid-rows-4">
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">nnsknsk</div>
+      </div>
     </div>
   )
 }
