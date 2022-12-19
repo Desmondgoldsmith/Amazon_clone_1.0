7763c2283d1b31cc2bc06d85eb559a2adb375e21
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 13:46:55 +0000

    fixing some bugs

--- Src/Components/Mobileview_Category.js
+++ Src/Components/Mobileview_Category.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
 
-function Mobileview_Category({title,image}) {
+function Mobileview_Category({title,image,link}) {
   return (
     <div className='bg-white p-1 w-[150px] rounded-md shrink-0 z-30 h-[200px] '>
        <p className='font-bold truncate text-sm'>{title}</p>
-       <Image 
+       <Image onClick = {link}
        src={image}
        alt={title}
        width={100}
        height={120}
-       className = "w-[200px] h-[165px]"  
+       className = "w-[200px] h-[165px] cursor-pointer"  
        />
     </div>
   )
