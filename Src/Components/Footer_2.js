f8b36f92ba5f7c32e99c7e9ed56131bc2b30164f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 17:54:22 +0000

    fixing some bugs

--- Src/Components/Footer_2.js
+++ Src/Components/Footer_2.js
@@ -4,7 +4,11 @@ function Footer_2() {
   return (
     <div className = "p-10 h-[300px] text-white bg-amazon_blue">
       <div className = "grid gap-6 grid-cols-7 grid-rows-4">
-      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">
+      <p className = "text-[12px] font-extrabold">Amazon Music</p>
+        <p className = "text-xs">Stream millions</p>
+        <p className = "text-xs">of songs</p>	
+      </div>
       <div className = "border border-red-600">nnsknsk</div>
       <div className = "border border-red-600">nnsknsk</div>
       <div className = "border border-red-600">nnsknsk</div>
