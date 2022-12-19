ab870d05f6a9cd9ca767fde091e0c89776710a7d
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 17:58:21 +0000

    fixing some bugs

--- Src/Components/Footer_2.js
+++ Src/Components/Footer_2.js
@@ -3,13 +3,17 @@ import React from 'react'
 function Footer_2() {
   return (
     <div className = "p-10 h-[300px] text-white bg-amazon_blue">
-      <div className = "grid gap-6 grid-cols-7 grid-rows-4">
+      <div className = "grid gap-6 grid-cols-7 grid-rows-4 ml-40 mr-40">
       <div className = "border border-red-600">
       <p className = "text-[12px] font-extrabold">Amazon Music</p>
-        <p className = "text-xs">Stream millions</p>
-        <p className = "text-xs">of songs</p>	
+        <p  className = "text-xs"><Link href = "/#">Stream millions</Link></p>
+        <p className = "text-xs"><Link href = "/#">of songs</Link></p>	
       </div>
-      <div className = "border border-red-600">nnsknsk</div>
+      <div className = "border border-red-600">
+      <p className = "text-[12px] font-extrabold"> Amazon Advertising</p>
+      <p className = "text-xs"><Link href = "/#">Find, attract, and</Link></p>
+      <p className = "text-xs"><Link href = "/#">engage customers</Link></p>
+      </div>
       <div className = "border border-red-600">nnsknsk</div>
       <div className = "border border-red-600">nnsknsk</div>
       <div className = "border border-red-600">nnsknsk</div>
