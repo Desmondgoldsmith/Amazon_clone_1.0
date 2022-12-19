363c28b61eb72a67399568b7ab3b79700d1c2f86
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-19 17:29:52 +0000

    fixing some bugs

--- Src/Components/Footer_1.js
+++ Src/Components/Footer_1.js
@@ -51,7 +51,7 @@ function Footer_1() {
       <hr className = "mb-3 mt-6 text-gray-200"/>
 
       <div className = "flex justify-center space-x-3 items-center">
-      <div onClick={()=>router.push('/')} className='mt-2 mr-5 items-center hover:border border border-transparent p-1 hover:border-solid hover:border-white cursor-pointer'>
+      <div onClick={()=>router.push('/')} className='mt-2 mr-10 items-center hover:border border border-transparent p-1 hover:border-solid hover:border-white cursor-pointer'>
          <Image src = 'https://pngimg.com/uploads/amazon/amazon_PNG25.png'
           width={100}
           height={20}
@@ -59,7 +59,7 @@ function Footer_1() {
          />
        </div>
               {/* language section */}
-      <div className=' mr-2 space-x-1   p-1 border border-white cursor-pointer   group relative'>
+      <div className=' mr-2 space-x-1 w-[120px] flex p-1 border border-white cursor-pointer   group relative'>
         <Image src='https://etc.usf.edu/clipart/72500/72519/72519_28_us_c_lg.gif'
           width={30}
           height={10}
@@ -71,93 +71,7 @@ function Footer_1() {
     </svg>
         </p>
           </button>
-    
-        {/* dropdown */}
-        <nav tabIndex={0} onKeyDown = {0} role = 'button' className="border z-50 border-gray-200 mt-2.5 bg-gray-100 invisible rounded w-60  absolute left-0 top-full transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1">
-            <ul className="py-1 text-black p-3 space-y-3">
-                <li className='flex items-center'>
-                   <p className='font-bold'>Change language</p>
-                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 hover:underline hover:text-blue-400">
-                        learn more 
-                    </Link>
-                </li>
-
-
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500' type="radio" id="" name="english" value="English" />
-                <label className='text-base'>English - EN</label>
-                </li>
-
-                <li>
-                  <hr className='mt-2 mb-2  border-1'/>
-                </li>
-
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500  ' type="radio" id="" name="spanish" value="Spanish" />
-                <label className = 'hover:underline hover:text-yellow-500 text-base' >español - ES</label>
-                </li>
-               
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500 ' type="radio" id="" name="arabic" value="Arabic" />
-                <label className = 'hover:underline hover:text-yellow-500 text-base' >العربية - AR</label>
-                </li>
-               
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500 ' type="radio" id="" name="deutsche" value="Deutsche" />
-                <label className = 'hover:underline hover:text-yellow-500 text-base' >Deutsch - DE</label>
-                </li>
-                
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500 ' type="radio" id="" name="hebrew" value="Hebrew" />
-                <label  className = 'hover:underline hover:text-yellow-500 text-base'>עברית - HE</label>
-                </li>
-                
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500 ' type="radio" id="" name="korea" value="Korea" />
-                <label className = 'hover:underline hover:text-yellow-500 text-base' >한국어 - KO</label>
-                </li>
-                
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500 ' type="radio" id="" name="portuguse" value="Portuguse" />
-                <label  className = 'hover:underline hover:text-yellow-500 text-base'>português - PT</label>
-                </li>
-               
-                <li className='flex items-center space-x-2'>
-                <input className='accent-yellow-500 ' type="radio" id="" name="china" value="China" />
-                <label>中文 (简体) - ZH</label>
-                </li>
-
-                 <li>
-                  <hr className='mt-2 mb-2'/>
-                </li>
-
-                <li className='flex items-center'>
-                   <p className='font-bold'>Change currency</p>
-                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 hover:underline hover:text-blue-400">
-                        learn more 
-                    </Link>
-                </li>
-
-                <li className='flex items-center justify-between'>
-                  <p>$ - USD - US Dollar</p>
-                  <p className='hover:underline  hover:text-blue-400'><Link href='#'>Change</Link></p>
-                </li>
-
-                <li className='flex items-center space-x-2'>
-                <Image src='https://etc.usf.edu/clipart/72500/72519/72519_28_us_c_lg.gif'
-                  width={20}
-                  height={8}
-                />
-                  <p>You are shopping on Amazon.com</p>
-                </li>
-
-                <li className='text-center text-blue-500'>
-                  <p className='hover:underline  hover:text-blue-400'><Link href = "#">Change country/region.</Link></p>
-
-                </li>
-
-            </ul>
-        </nav>
+           <Nav_dropdown/>
       </div>
       {/* us-dollar */}
       <div className=' mr-2 space-x-1   p-1 border border-white cursor-pointer   group relative'>
