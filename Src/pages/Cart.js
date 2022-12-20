da6a4445f3a7c47a17cb6335ada28bbc603f979a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-20 15:10:21 +0000

    Checkout page

--- Src/pages/Cart.js
+++ Src/pages/Cart.js
@@ -11,6 +11,7 @@ import Footer_2 from '../Components/Footer_2'
 import Footer from '../Components/Footer[mobile_1]'
 import MobileFooter2 from '../Components/MobileFooter2'
 import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios'
 
 const stripePromise = loadStripe(process.env.stripe_public_key)
 
