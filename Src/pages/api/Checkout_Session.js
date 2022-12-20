d6caaf8f387b0180e574d318844480aa43c34a6f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-20 15:33:49 +0000

    Checkout page

--- Src/pages/api/Checkout_Session.js
+++ Src/pages/api/Checkout_Session.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 export default async (req,res) =>{
-   const {items,email} = req.body
-
-   console.log(item)
-   console.log(email)
+   const {items,email} = req.body,
+   const transformedItems = items.map(item => ({
+    
+   })) 
 }
