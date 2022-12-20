ce6a0239228beee3776b92b14ed97885e2b8fd80
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-20 16:08:34 +0000

    Checkout page

--- Src/pages/api/Checkout_Session.js
+++ Src/pages/api/Checkout_Session.js
@@ -2,12 +2,21 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 export default async (req,res) =>{
    const {items,email} = req.body
    const transformedItems = items.map(item => ({
-     price_data:{
+    description : item.description,
+    quantity : 1 ,
+    price_data:{
         currency : 'usd',
+        unit_amount: item.price * 100,
         product_data:{
             name: item.title,
             images:[item.image]
         }
      }
    })) 
+
+   const session = await stripe.checkout.session.create({
+    payment_method_types : ['card'], //select payment methods
+    line_items : transformedItems //get a list of items in cart
+
+   })
 }
