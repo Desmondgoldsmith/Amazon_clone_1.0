caa203806c8d6bc0e502331c67eaeb7abacd59b3
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-20 00:40:28 +0000

    Finally Done ! 🥰

--- Src/Components/Footer_1.js
+++ Src/Components/Footer_1.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Nav_dropdown from './Nav_dropdown'
-import useRouter from 'next/router'
+import {useRouter} from 'next/router'
 function Footer_1() {
   const router = useRouter()
   return (
