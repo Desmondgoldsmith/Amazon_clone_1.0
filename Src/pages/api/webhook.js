49d598a0bd0f2191ce39961f09a95084187007e7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-21 11:50:53 +0000

    Working on the checkout page

--- Src/pages/api/webhook.js
+++ Src/pages/api/webhook.js
@@ -1,9 +1,15 @@
 import {buffer} from 'micro'
 import * as admin from 'firebase-admin'
+
+// connect to firebase (backend)
 cost serviceAccount = require('../../../permission.json')
 const app = !admin.apps.length ? admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
-}) : admin.app()
+}) : admin.app();
+
+// connect to stripe
+const stripe = require('stripe')
+
 export default async (req,res)=>{
     if(req.method === 'POST'){
     }
