733b1b300fed23ee3ca0a5944808b0f78bb611b8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-21 14:14:35 +0000

    Working on the cart page

--- next.config.js
+++ next.config.js
@@ -16,7 +16,8 @@ module.exports = {
   },
 
   env:{
-     stripe_public_key:process.env.STRIPE_PUBLIC_KEY
+     stripe_public_key:process.env.STRIPE_PUBLIC_KEY,
+     STRIPE_SIGNING_SECRET:process.env.STRIPE_SIGNING_SECRET
   }
 }
 
