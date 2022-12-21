c231b473d8c6e56d6d31244e0dd0cc0702549a08
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-21 17:33:16 +0000

    Working on the cart page

--- Src/pages/success.js
+++ Src/pages/success.js
@@ -1,11 +1,14 @@
 import react from 'react'
-
+import Header from '../Components/Header';
 
 
 function success(){
     return(
         <div>
           <Header/>
+          <div w-full justify-center>
+
+          </div>
         </div>
     )
 }
