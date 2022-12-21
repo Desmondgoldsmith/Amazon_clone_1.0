90e5db4a827b9304a0075a5db1c858c954ce0599
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-21 22:53:38 +0000

    Working on the cart page

--- Src/pages/success.js
+++ Src/pages/success.js
@@ -6,8 +6,9 @@ function success(){
     return(
         <div>
           <Header/>
-          <div className = "ml-0 mr-0 w-full md:ml-[300px] md:mr-[300px] lg:ml-[25%] lg:mr-[25%]  md:w-[600px] lg:w-[800px]  border-2 border-red-600">
-             <p>Money</p>   
+          <div className = "ml-0 mr-0 w-full md:ml-[300px] md:mr-[300px] lg:ml-[300px] lg:mr-[350px]  md:w-[600px] lg:w-[800px]  border-2 border-red-600">
+             <p>Thank you, Your order has been confirmed !</p>   
+             <p></p>   
           </div>
         </div>
     )
