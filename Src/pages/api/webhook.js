1447687fb47be2c165bd148c709942f1c023863e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-21 11:40:18 +0000

    Working on the checkout page

--- Src/pages/api/webhook.js
+++ Src/pages/api/webhook.js
@@ -1,7 +1,10 @@
+import {buffer} from 'micro'
+import * as admin from 'firebase-admin'
+
 export default async (req,res)=>{
     if(req.method === 'POST'){
 
     }if(req.method === 'GET'){
-        
+
     }
 }
