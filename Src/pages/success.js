aae8e31fe6ba83c797602a3c9da614c1da672d15
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-21 23:12:19 +0000

    Working on the cart page

--- Src/pages/success.js
+++ Src/pages/success.js
@@ -7,6 +7,8 @@ function success(){
         <div>
           <Header/>
           <div className = "ml-0 mr-0 w-full md:ml-[300px] md:mr-[300px] lg:ml-[300px] lg:mr-[350px]  md:w-[600px] lg:w-[800px]  border border-green-400 p-4 mt-10">
+            <div className = "border border-green-300"> </div>
+            <div className = "border border-green-200  "> </div>
              <p className ="text-green-600 text-[20px] text-center font-bold ">Thank you, Your order has been confirmed !</p>   
              <p className = "text-center text-sm "> please chech your email for order confirmation and detailed delivery</p>   
           </div>
