b859dbf9b61dfc126d69dfb3e9d20882014d9e54
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-22 22:39:27 +0000

    Working on the cart page

--- Src/Components/Footer_1.js
+++ Src/Components/Footer_1.js
@@ -10,43 +10,43 @@ function Footer_1() {
      <div className = "  pl-40 pr-40  grid gap-10 grid-cols-4">
      <div  className = "space-y-1">
       <p className = "font-extrabold text-[18px]">Get to Know Us</p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Careers</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Blog</Link></p>
-       <p className = "cursor-pointer text-sm"> <Link href = "/#" >About Amazon</Link></p>
-       <p className = "cursor-pointer text-sm"> <Link href = "/#" >Investor Relations</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Amazon Devices</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Amazon Science</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Careers</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Blog</Link></p>
+       <p className = "cursor-pointer text-xs"> <Link href = "/#" >About Amazon</Link></p>
+       <p className = "cursor-pointer text-xs"> <Link href = "/#" >Investor Relations</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Amazon Devices</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Amazon Science</Link></p>
       </div>
       <div className = "space-y-1">
         	
       <p className = "font-extrabold text-[18px]">Make Money with Us</p>
-        <p className = "cursor-pointer text-sm"><Link href = "/#" >Sell products on Amazon</Link></p>
-        <p className = "cursor-pointer text-sm"><Link href = "/#" >Sell on Amazon Business</Link></p>
-        <p className = "cursor-pointer text-sm"><Link href = "/#" >Sell apps on Amazon</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Become an Affiliate</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Advertise Your Products</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Self-Publish with Us</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Host an Amazon Hub</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > ›See More Make Money with Us</Link></p>
+        <p className = "cursor-pointer text-xs"><Link href = "/#" >Sell products on Amazon</Link></p>
+        <p className = "cursor-pointer text-xs"><Link href = "/#" >Sell on Amazon Business</Link></p>
+        <p className = "cursor-pointer text-xs"><Link href = "/#" >Sell apps on Amazon</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Become an Affiliate</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Advertise Your Products</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Self-Publish with Us</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Host an Amazon Hub</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > ›See More Make Money with Us</Link></p>
       </div>
       <div className = "space-y-1">
       <p className = "font-extrabold text-[18px]"> Amazon Payment Products</p>
-        <p className = "cursor-pointer text-sm"><Link href = "/#" >Amazon Business Card</Link></p>
-        <p className = "cursor-pointer text-sm"><Link href = "/#" >Shop with Points</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Reload Your Balance</Link></p>
-        <p className = "cursor-pointer text-sm"><Link href = "/#" >Amazon Currency Converter</Link></p>
+        <p className = "cursor-pointer text-xs"><Link href = "/#" >Amazon Business Card</Link></p>
+        <p className = "cursor-pointer text-xs"><Link href = "/#" >Shop with Points</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Reload Your Balance</Link></p>
+        <p className = "cursor-pointer text-xs"><Link href = "/#" >Amazon Currency Converter</Link></p>
       </div>
       <div className = "space-y-1">
       	
       <p className = "font-extrabold text-[18px]">Let Us Help You</p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Amazon and COVID-19</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Your Account</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Your Orders</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Shipping Rates & Policies</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Returns & Replacements</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Manage Your Content and Devices</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Amazon Assistant</Link></p>
-       <p className = "cursor-pointer text-sm"><Link href = "/#" > Help</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Amazon and COVID-19</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Your Account</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Your Orders</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Shipping Rates & Policies</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Returns & Replacements</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Manage Your Content and Devices</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Amazon Assistant</Link></p>
+       <p className = "cursor-pointer text-xs"><Link href = "/#" > Help</Link></p>
       </div>
      </div>
       {/* horizontal line*/}
