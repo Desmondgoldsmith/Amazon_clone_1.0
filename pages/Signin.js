fd8f16df2a61d5b3fdb2ec0da6520ce88e068f06
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-12-23 20:46:50 +0000

    Done

--- pages/Signin.js
+++ pages/Signin.js
@@ -31,7 +31,7 @@ return (
     <div className='border p-2 md:p-0 lg:p-0 w-[380px] justify-self-center pl-2 pr-2 -mt-[20px]'>
         <h1 className='text-[30px]'>Create account</h1>
         <p className = "text-center text-xs text-red-600 font-extrabold">Please sign in only with google </p>
-        <form className='mt-3'> 
+        <div className='mt-3'> 
         <label  className='font-bold grid mb-2'>Your name</label>
         <input className='rounded-md border w-[360px] text-[15px] border-gray-400 grid p-1' type="text" id='name' name='name' placeholder='First and last name' />
 
@@ -49,7 +49,7 @@ return (
         <button onClick={!session ? signIn : signOut} className='bg-white w-[360px] h-[35px] hover:bg-black hover:text-white mt-3 rounded-md'>{session ? "Sign out of Amazon Clone" : "Sign-in with Google"}</button>
          <p className='text-xs mt-3'>By creating an account, you agree to Amazon's <span className='text-blue-500'>Conditions of Use</span>  and <span className='text-blue-500'>Privacy Notice</span>.</p>
          
-         </form>
+         </div>
          <hr className='mt-4 mb-4 w-[250px] justify-self-center'/>
          <p  className='text-sm'>Already have an account? <span className='text-blue-500'>Sign in</span> </p>
          <p className='text-sm mb-3'>Buying for work? <span className='text-blue-500'>Create a free business account</span> </p>
